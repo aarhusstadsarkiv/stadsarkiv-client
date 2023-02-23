5305650962e2b9ef2ddfb511c18f129f7cb8408f
Dennis Iversen <deiv@aarhus.dk>
2023-02-23 15:05:19 +0100

    set correct static path

--- stadsarkiv_client/routes.py
+++ stadsarkiv_client/routes.py
@@ -3,12 +3,15 @@ from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
 from stadsarkiv_client.endpoints import auth, pages
 
+# get current directory
+import os
+static_dir = os.path.dirname(os.path.abspath(__file__)) + '/static'
 
 routes = [
     Route('/', endpoint=pages.home, name='home'),
     Route('/about', endpoint=pages.about, name='about'),
     Route('/admin', endpoint=pages.admin, name='admin'),
-    Mount('/static', StaticFiles(directory='static'), name='static'),
+    Mount('/static', StaticFiles(directory=static_dir), name='static'),
     Route('/auth/login', endpoint=auth.get_login, name='login'),
     Route('/auth/post-login', endpoint=auth.post_login,
           name='post_login', methods=['POST']),
