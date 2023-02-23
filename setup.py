fbf716b596a505db5c7d7d8eff148da1e8045682
Dennis Iversen <deiv@aarhus.dk>
2023-02-23 15:12:19 +0100

    setup. add templates

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     author_email='deiv@aarhus.dk',
     license='MIT',
     packages=find_packages(exclude=("tests",)),
+    package_data={"stadsarkiv-client": ["templates/*.html"]},
     install_requires=REQUIREMENTS,
 
     classifiers=[
