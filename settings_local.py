707c6fa7e21c72cc4b1c6131bba89eecebc35dc7
Dennis Iversen <deiv@aarhus.dk>
2023-02-27 11:07:57 +0100

    add local settings and local static dir

---
+++ settings_local.py
@@ -0,0 +1,5 @@
+settings_local = {
+    "templates_local": "./templates", # Override templates
+    "static_local": "./static", # Change static folder
+    "static_extra": "./static_extra", # Add extra static folder
+}
