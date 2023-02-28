7907e52369f9770dc37bf119135de921c502c220
Dennis Iversen <deiv@aarhus.dk>
2023-02-28 10:26:00 +0100

    mv serve to test command

--- stadsarkiv_client/test_serve.py
+++ stadsarkiv_client/test_serve.py
@@ -1,11 +1,16 @@
 import click
-from .cli import serve
+import uvicorn
+import os
+from stadsarkiv_client.utils.dynamic_settings import settings
+from stadsarkiv_client.utils.logging import log
 
 @click.command()
 @click.option('--reload', default=True, help='Reload uvicorn on changes.')
 @click.option('--port', default=5555, help='Server port.')
 def run(reload: bool, port: int):
-    serve(reload=reload, port=port)
+    log.debug(os.getenv('ENVIRONMENT'))
+    log.debug(settings)
 
-""" if __name__ == "__main__":
-    run() """
+    uvicorn.run("stadsarkiv_client.app:app",
+                reload=reload, port=port, log_level="info")
+
