8f4c365fb1eb365227ddd22de1bc35658d1a7d9f
Dennis Iversen <deiv@aarhus.dk>
2023-02-28 10:38:04 +0100

    clean up

--- stadsarkiv_client/__main__.py
+++ stadsarkiv_client/__main__.py
@@ -1,11 +1,4 @@
-import click
-from .cli import serve
-
-@click.command()
-@click.option('--reload', default=True, help='Reload uvicorn on changes.')
-@click.option('--port', default=5555, help='Server port.')
-def run(reload: bool, port: int):
-    serve(reload=reload, port=port)
+from stadsarkiv_client.commands.serve import run
 
 if __name__ == "__main__":
     run()
