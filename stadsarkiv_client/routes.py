9453c0e9b799a68be0934b40e1883b094119af9d
Dennis Iversen <deiv@aarhus.dk>
2023-02-28 12:13:27 +0100

    multiple static dirs. This allows override

--- stadsarkiv_client/routes.py
+++ stadsarkiv_client/routes.py
@@ -1,25 +1,38 @@
 import typing
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
-from .endpoints import auth, testing, home, pages
+from .endpoints import auth, testing, pages
 import os
 from stadsarkiv_client.utils.dynamic_settings import settings
-from stadsarkiv_client.utils.logging import log
+# from stadsarkiv_client.utils.logging import log
+from starlette.staticfiles import PathLike, StaticFiles
+
+# Override static files in order to load multiple static folders
+class MultiStaticFiles(StaticFiles):
+    def __init__(self, directories: typing.List[PathLike] = [], **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.all_directories = self.all_directories + directories
 
 
-static_dir = os.path.dirname(os.path.abspath(__file__)) + '/static'
+
+static_dir_list: typing.List[PathLike] = []
+
 if "static_local" in settings:
     if os.path.exists(settings["static_local"]):
-        static_dir = settings["static_local"]
+        static_dir_local = settings["static_local"]
+        static_dir_list.append(static_dir_local)
+
+# Module static files
+static_dir = os.path.dirname(os.path.abspath(__file__)) + '/static'
+static_dir_list.append(static_dir)
 
 
 routes = [
     # Route('/', endpoint=home.index, name='home'),
-    Mount('/static', StaticFiles(directory=static_dir), name='static'),
+    Mount('/static', MultiStaticFiles(directories=static_dir_list), name='static'),
     Route('/auth/login', endpoint=auth.get_login, name='login'),
     Route('/auth/post-login', endpoint=auth.post_login, name='post_login', methods=['POST']),
     Route('/auth/register', endpoint=auth.get_register, name='register'),
-    # Route('/pages/register', endpoint=auth.get_register, name='register'),
     Route('/test', endpoint=testing.test, name='test'),
 ]
 
