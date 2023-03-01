c073f5dad1e1955a307e1fbc090b42e741705163
Dennis Iversen <deiv@aarhus.dk>
2023-03-01 13:28:44 +0100

    add translation to login.html

--- stadsarkiv_client/locales/da.py
+++ stadsarkiv_client/locales/da.py
@@ -1,13 +1,12 @@
 da = {
+    "Email": "E-mail",
     "Email needs to be correct. Password needs to be at least 8 characters long.": "Email er ikke korrekt. Password skal v\u00e6re mindst 8 tegn langt.",
-    "Email or password is incorrect. Or your user has not been activated.": "Email er ikke korrekt. Password skal v\u00e6re mindst 8 tegn langt.",
+    "Email or password is incorrect. Or your user has not been activated.": "Email eller password er ikke korrekt. Eller din konto er ikke blevet aktiveret.",
     "Login": "Log ind",
     "Logout": "Log ud",
     "New user": "Ny bruger",
+    "Password": "Password",
     "User already exists. Try to login instead.": "Bruger findes allerede. Pr\u00f8v at logge ind i stedet.",
-    "You have been logged in": "Du er logget ind.",
     "You have been logged in.": "You have been logged in.",
-    "You have been logged out": "You have been logged out",
-    "You have been logged out.": "You have been logged out.",
-    "You have been registered.": "You have been registered."
+    "You have been logged out.": "You have been logged out."
 }
--- stadsarkiv_client/locales/en.py
+++ stadsarkiv_client/locales/en.py
@@ -1,9 +1,9 @@
 en = {
+    "Email": "Email",
+    "Email or password is incorrect. Or your user has not been activated.": "Email or password is incorrect. Or your user has not been activated.",
     "Login": "Login",
     "Logout": "Logout",
     "New user": "New user",
+    "Password": "Password",
     "User already exists. Try to login instead.": "User already exists. Try to login instead.",
-    "You have been logged in.": "You have been logged in.",
-    "You have been logged out.": "You have been logged out.",
-    "You have been registered.": "You have been registered."
 }
