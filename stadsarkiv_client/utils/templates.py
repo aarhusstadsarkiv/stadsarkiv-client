7cd0d27b6f759cbb921cebf6828c2d380fd14416
Dennis Iversen <deiv@aarhus.dk>
2023-03-01 13:28:21 +0100

    add translate function to templates

--- stadsarkiv_client/utils/templates.py
+++ stadsarkiv_client/utils/templates.py
@@ -1,9 +1,10 @@
 import typing
 import os
-from jinja2 import FileSystemLoader 
+from jinja2 import FileSystemLoader, Environment
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 from .dynamic_settings import settings
+from .translate import translate
 
 # Asynchronous functions as context processors are not supported.
 def app_context(request: Request) -> typing.Dict[str, typing.Any]:
@@ -12,19 +13,21 @@ def app_context(request: Request) -> typing.Dict[str, typing.Any]:
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/../templates/"
 
-templates = []
+template_dirs = []
 
 # Load local templates first
 if "templates_local" in settings:
-    templates.append(settings["templates_local"])
+    template_dirs.append(settings["templates_local"])
 
 # Load default templates
-templates.append(dir_path)
+template_dirs.append(dir_path)
 
 
-loader = FileSystemLoader(templates)
+loader = FileSystemLoader(template_dirs)
 
 
 templates = Jinja2Templates(
-    directory='', context_processors=[app_context], loader=loader
+    directory='', context_processors=[app_context], loader=loader,
 )
+
+templates.env.globals.update(translate=translate)
