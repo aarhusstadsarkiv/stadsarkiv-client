acccfa1c94a863903c6909cc05342467c5d5bfb2
Dennis Iversen <deiv@aarhus.dk>
2023-03-02 14:50:18 +0100

    add workers to command

--- stadsarkiv_client/commands/serve.py
+++ stadsarkiv_client/commands/serve.py
@@ -8,7 +8,7 @@ from stadsarkiv_client.utils.logging import log
 @click.command()
 @click.option('--reload', default=True, help='Reload uvicorn on changes.')
 @click.option('--port', default=5555, help='Server port.')
-@click.option('--workers', default=4, help='Number of workers.')
+@click.option('--workers', default=1, help='Number of workers.')
 def run(reload: bool, port: int, workers: int):
     log.debug(os.getenv('ENVIRONMENT'))
     log.debug(settings)
