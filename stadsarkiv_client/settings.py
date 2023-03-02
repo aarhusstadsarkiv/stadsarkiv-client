2c7738e16d7e15587a830b260f51702216dfd012
Dennis Iversen <deiv@aarhus.dk>
2023-03-02 12:27:15 +0100

    add default pages and menu

--- stadsarkiv_client/settings.py
+++ stadsarkiv_client/settings.py
@@ -35,4 +35,45 @@ settings = {
         "secure": cookie_secure,
         "samesite": "lax"
     },
+    "main_menu": [
+        {
+            "name": "home",
+            "title": "Hjem"
+        },
+        {
+            "name": "about",
+            "title": "Om"
+        },
+        {
+            "name": "login",
+            "title": "Log ind"
+        },
+        {
+            "name": "logout",
+            "title": "Log ud"
+        },
+        {
+            "name": "register",
+            "title": "Register"
+        },
+        {
+            "name": "profile",
+            "title": "Profil"
+        },
+
+    ],
+    "pages": [
+        {
+            "name": "home",
+            "title": "Hjem",
+            "page": "pages/home.html",
+            "url": "/"
+        },
+        {
+            "name": "about",
+            "title": "Om",
+            "page": "pages/about.html",
+            "url": "/about"
+        }
+    ]
 }
