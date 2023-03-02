fb58f49e03e44b638b3472c801409e13cefcb957
Dennis Iversen <deiv@aarhus.dk>
2023-03-02 14:22:07 +0100

    clean up. remove not used

--- stadsarkiv_client/endpoints/pages.py
+++ stadsarkiv_client/endpoints/pages.py
@@ -1,10 +1,9 @@
 from starlette.requests import Request
 from stadsarkiv_client.utils.templates import templates
 from stadsarkiv_client.utils.context import get_context
-from stadsarkiv_client.utils.logging import log
 
- 
+
 async def default(request: Request):
 
     context = get_context(request)
-    return templates.TemplateResponse('pages/about.html', context)
+    return templates.TemplateResponse('pages/default.html', context)
