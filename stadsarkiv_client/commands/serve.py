b56a3e18e111c9bb031c62d0a4ba38e18f5e95b2
Dennis Iversen <deiv@aarhus.dk>
2023-03-02 14:30:22 +0100

    clean up. Move cli. not used

--- stadsarkiv_client/commands/serve.py
+++ stadsarkiv_client/commands/serve.py
@@ -4,13 +4,14 @@ import os
 from stadsarkiv_client.utils.dynamic_settings import settings
 from stadsarkiv_client.utils.logging import log
 
+
 @click.command()
 @click.option('--reload', default=True, help='Reload uvicorn on changes.')
 @click.option('--port', default=5555, help='Server port.')
-def run(reload: bool, port: int):
+@click.option('--workers', default=4, help='Number of workers.')
+def run(reload: bool, port: int, workers: int):
     log.debug(os.getenv('ENVIRONMENT'))
     log.debug(settings)
 
     uvicorn.run("stadsarkiv_client.app:app",
-                reload=reload, port=port, log_level="info")
-
+                reload=reload, port=port, workers=workers, log_level="info")
