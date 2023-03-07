640faad7e715f5112e23649f03f3a4739800bb22
Dennis Iversen <deiv@aarhus.dk>
2023-03-07 14:35:28 +0100

    pretty print settings in development mode

--- stadsarkiv_client/app.py
+++ stadsarkiv_client/app.py
@@ -3,6 +3,7 @@ from stadsarkiv_client.routes import routes
 from stadsarkiv_client.utils.middleware import session_middleware, session_autoload_middleware
 from stadsarkiv_client.hooks.manager import get_plugin_manager
 import os
+import json
 import pprint
 from stadsarkiv_client.utils.dynamic_settings import settings
 from stadsarkiv_client.utils.logging import get_log
@@ -14,7 +15,9 @@ pm.hook.before_render_template()  # type: ignore
 
 # log debug environment
 log.debug("Environment: " + str(os.getenv('ENVIRONMENT')))
-log.debug(pprint.pprint(settings))
+# log.debug(pprint.pprint(settings))
+
+log.debug(json.dumps(settings, sort_keys=True, indent=4))
 
 
 app = Starlette(debug=True, middleware=[session_middleware, session_autoload_middleware], routes=routes)
