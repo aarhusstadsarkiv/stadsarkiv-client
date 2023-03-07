3d9b82f5e1f92724a1b8d27438dd2944de82a296
Dennis Iversen <deiv@aarhus.dk>
2023-03-07 12:55:26 +0100

    info about local language file

--- stadsarkiv_client/utils/translate.py
+++ stadsarkiv_client/utils/translate.py
@@ -8,7 +8,7 @@ try:
     from language import language as language_local
     log.info("Loaded local language file: language.py")
 except ImportError:
-    log.info("local settings file NOT loaded: settings.py")
+    log.info("Local language file NOT loaded: language.py")
     language_local = None
 
 
