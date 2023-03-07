6c74dea4b2536d146c715949b24e8a088025a41d
Dennis Iversen <deiv@aarhus.dk>
2023-03-07 11:19:52 +0100

    plugins file may exist or not exist. Handle graceful.

--- stadsarkiv_client/hooks/manager.py
+++ stadsarkiv_client/hooks/manager.py
@@ -1,17 +1,24 @@
 from stadsarkiv_client.hooks import hook_specs
 import pluggy
-import plugins
+
 import inspect
 import sys
+
+try:
+    import plugins
+except ImportError:
+    plugins = None
 
 
 def get_plugin_manager() -> pluggy.PluginManager:
     pm = pluggy.PluginManager("stadsarkiv_client")
     pm.add_hookspecs(hook_specs)
-    pm.register(plugins)
 
-    for _, obj in inspect.getmembers(sys.modules[plugins.__name__]):
-        if inspect.isclass(obj):
-            pm.register(obj)
+    if plugins:
+        pm.register(plugins)
+
+        for _, obj in inspect.getmembers(sys.modules[plugins.__name__]):
+            if inspect.isclass(obj):
+                pm.register(obj)
 
     return pm
