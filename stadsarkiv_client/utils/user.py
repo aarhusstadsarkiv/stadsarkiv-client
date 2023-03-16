2d087e100d7640d19f584a646543b6a6b42b99af
Dennis Iversen <deiv@aarhus.dk>
2023-03-16 12:30:23 +0100

    add utils user.py

--- stadsarkiv_client/utils/user.py
+++ stadsarkiv_client/utils/user.py
@@ -1,10 +1,30 @@
 from starlette.requests import Request
 from stadsarkiv_client.utils.logging import get_log
-
+from stadsarkiv_client.utils import flash
 
 log = get_log()
 
 
-async def get_user(request: Request):
-    log.debug("get_user")
+async def get_user(request: Request, level: int = 0):
+
+    log.debug('get_user()')
     pass
+
+
+async def set_user_cookie(request: Request, cookie_dict: dict):
+
+    request.session["logged_in"] = True
+    request.session["login_type"] = "cookie"
+    request.session["_auth"] = cookie_dict["_auth"]
+
+    log.debug('user logged in')
+
+
+async def set_user_jwt(request: Request, bearer_token: dict):
+
+    request.session["logged_in"] = True
+    request.session["access_token"] = bearer_token["access_token"]
+    request.session["token_type"] = bearer_token["token_type"]
+    request.session["login_type"] = "jwt"
+
+    log.debug('user logged in')
