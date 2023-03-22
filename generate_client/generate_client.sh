21fb6d17b31f303a75815cfc8c5591a3469b91b7
Dennis Iversen <deiv@aarhus.dk>
2023-03-22 10:54:43 +0100

    update client generation

--- generate_client/generate_client.sh
+++ generate_client/generate_client.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-rm -rf stadsarkivet-client
-openapi-python-client generate --url http://localhost:8000/v1/openapi.json --config config.yml
-# --config config.yml 
+
+# check if directory exists 'openaws-client'
+# if not clone it
+if [ ! -d "openaws-client" ]; then
+    git clone git@github.com:aarhusstadsarkiv/openaws-client.git
+fi
+
+# Mv git repo to tmp
+mv openaws-client openaws-client-tmp
+
+# generate new client
+openapi-python-client generate --config config.yml --url http://localhost:8000/v1/openapi.json 
+
+# copy new client code on top of old client
+cp -rf openaws-client/* openaws-client-tmp/
+rm -rf openaws-client
+mv openaws-client-tmp openaws-client
