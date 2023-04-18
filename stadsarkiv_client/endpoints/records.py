53c12f3fe51541d3a89d7850881fdfcbe19fc050
Dennis Iversen <deiv@aarhus.dk>
2023-04-18 15:03:00 +0200

    begin parsing fields in records

--- stadsarkiv_client/endpoints/records.py
+++ stadsarkiv_client/endpoints/records.py
@@ -6,6 +6,7 @@ from stadsarkiv_client.core.translate import translate
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core import api
 import json
+from stadsarkiv_client.core.alter_records import alter_record
 from stadsarkiv_client.core.openaws import (
     SchemaRead,
     EntityRead,
@@ -42,14 +43,19 @@ async def get_record_view(request: Request):
     try:
         record: RecordsIdGet = await api.record_read(request)
         record_dict = record.to_dict()
+        record_dict = alter_record(record_dict)
 
         record_json = json.dumps(record_dict, indent=4, ensure_ascii=False)
-
-        context_values = {"title": translate("Entity"), "record": record_json}
+        context_values = {
+            "title": translate("Entity"),
+            "record_dict": record_dict,
+            "record_json": record_json,
+        }
 
         context = await get_context(request, context_values=context_values)
         return templates.TemplateResponse("records/record.html", context)
 
     except Exception as e:
         # for sure this is a 404
+        log.exception(e)
         raise HTTPException(404, detail=str(e), headers=None)
