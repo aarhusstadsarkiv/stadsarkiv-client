9f5047a95a02ea70bd2fb54b5df07f2f9a9273ba
Dennis Iversen <deiv@aarhus.dk>
2023-04-21 12:26:23 +0200

    sort record sections according to lists. Refactor.

--- stadsarkiv_client/endpoints/records.py
+++ stadsarkiv_client/endpoints/records.py
@@ -6,7 +6,7 @@ from stadsarkiv_client.core.translate import translate
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core import api
 import json
-from stadsarkiv_client.core.alter_records import alter_record
+from stadsarkiv_client.core import alter_record
 from stadsarkiv_client.core.openaws import (
     SchemaRead,
     EntityRead,
@@ -39,69 +39,16 @@ async def get_records_search_results(request: Request):
         raise HTTPException(404, detail=str(e), headers=None)
 
 
-def get_sections(record_dict: dict):
-
-    abstract = ['collectors', 'content_types', 'creators', 'date_from', 'curators', 'id']
-    description = ['heading', 'summary', 'collection', 'series_normalized', 'subjects']
-    copyright = ['copyright_status']
-    relations = ['organisations', 'locations']
-    copyright_extra = ['contractual_status', 'other_legal_restrictions']
-    availability = ['availability']
-    media = ['representations']
-
-    sections = {
-        "abstract": {}, "description": {}, "copyright": {}, "relations": {},
-        "copyright_extra": {}, "availability": {}, "download": {}, "other": {}
-    }
-
-    for key, value in record_dict.items():
-        if key in abstract:
-            sections['abstract'][key] = value
-        elif key in description:
-            sections['description'][key] = value
-        elif key in copyright:
-            sections['copyright'][key] = value
-        elif key in relations:
-            sections['relations'][key] = value
-        elif key in copyright_extra:
-            sections['copyright_extra'][key] = value
-        elif key in availability:
-            sections['availability'][key] = value
-        elif key in media:
-            sections['download'][key] = value
-
-    return sections
-
-
-def get_record_title(record_dict: dict):
-    title = record_dict['heading']
-    if title:
-        return title
-    else:
-        return record_dict['id']
-
-
-def get_record_image(record_dict: dict):
-    image = None
-    try:
-        if record_dict['representations']['record_type'] == 'image':
-            image = record_dict['representations']['record_image']
-    except KeyError:
-        pass
-
-    return image
-
-
 async def get_record_view(request: Request):
     try:
         record: RecordsIdGet = await api.record_read(request)
         record_dict = record.to_dict()
-        record_dict = alter_record(record_dict)
-        record_sections = get_sections(record_dict)
+        record_dict = alter_record.alter_record(record_dict)
+        record_sections = alter_record.get_sections(record_dict)
         record_sections_json = json.dumps(record_sections, indent=4, ensure_ascii=False)
         context_values = {
-            "title": get_record_title(record_dict),
-            "image": get_record_image(record_dict),
+            "title": alter_record.get_record_title(record_dict),
+            "image": alter_record.get_record_image(record_dict),
             "record_sections": record_sections,
             "record_sections_json": record_sections_json,
         }
