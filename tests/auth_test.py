078944eed84860128f8daae96c8fb4812d85424b
Dennis Iversen <deiv@aarhus.dk>
2023-06-01 14:51:10 +0200

    clean up tests

---
+++ tests/auth_test.py
@@ -0,0 +1,45 @@
+from stadsarkiv_client.app import app
+import sys
+
+from starlette.responses import HTMLResponse
+from starlette.testclient import TestClient
+
+import os
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class TestAuth(unittest.TestCase):
+
+    def test_home(self):
+        client = TestClient(app)
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_found(self):
+        client = TestClient(app)
+        response = client.get("/not_found")
+        self.assertEqual(response.status_code, 404)
+
+    def test_login(self):
+        client = TestClient(app)
+        response = client.get("/auth/login")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_post_correct(self):
+        client = TestClient(app)
+        response = client.post("/auth/post-login-jwt", data={"username": "dennis.iversen@gmail.com", "password": "iversen1234"})
+        self.assertEqual(response.status_code, 200)
+
+    def _login_post_incorrect(self):
+        client = TestClient(app)
+        client.post("/auth/post-login-jwt", data={"username": "dennis.iversens@gmail.com", "password": "iversen1234"})
+
+    def test_login_post_incorrect(self):
+        client = TestClient(app)
+        client = TestClient(app)
+        client.post("/auth/post-login-jwt", data={"username": "dennis.iversens@gmail.com", "password": "iversen1234"})
+        
+        
+        # self.assertEqual(response.status_code, 200)
