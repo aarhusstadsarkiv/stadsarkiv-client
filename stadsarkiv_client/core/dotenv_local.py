21d700c1bbf52df60356aee18031b5fee2a02e99
Dennis Iversen <deiv@aarhus.dk>
2023-06-13 15:29:32 +0200

    add example .env. Simpælify dotenv_local logging

--- stadsarkiv_client/core/dotenv_local.py
+++ stadsarkiv_client/core/dotenv_local.py
@@ -11,10 +11,9 @@ def load():
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     env_dist = dir_path + "/.env-dist"
     load_dotenv(env_dist)
-    log.info(f"Loaded .env-dist file from {env_dist}")
+    log.info("Loaded .env-dist file")
 
     if os.path.exists(".env"):
-
         load_dotenv(override=True)
         log.info("Local .env file loaded. Will override .env-dist settings")
     else:
