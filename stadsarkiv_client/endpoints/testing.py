7715c0ba813c05761f9ef7192b2c56b197f18820
Dennis Iversen <deiv@aarhus.dk>
2023-06-20 10:04:50 +0200

    add testing endpoint for new macros

--- stadsarkiv_client/endpoints/testing.py
+++ stadsarkiv_client/endpoints/testing.py
@@ -4,6 +4,8 @@ from stadsarkiv_client.core.context import get_context
 from stadsarkiv_client.core import dynamic_settings
 import json
 from stadsarkiv_client.core.logging import get_log
+from stadsarkiv_client.core import api
+
 
 log = get_log()
 
@@ -14,3 +16,15 @@ async def test(request: Request):
     context_variables = {"settings": settings_json, "title": "Test"}
     context = await get_context(request, context_variables)
     return templates.TemplateResponse("testing/test.html", context)
+
+
+async def test_entitites_macro(request: Request):
+    context_variables = {"title": "Test entities macro"}
+
+    record_id = "000110308"
+
+    record = await api.proxies_record_get_by_id(record_id)
+    log.debug(record)
+
+    context = await get_context(request, context_variables)
+    return templates.TemplateResponse("testing/test_entities_macro.html", context)
