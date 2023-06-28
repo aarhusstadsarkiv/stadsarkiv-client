7f26da8900aad68998b4260896f1c5ce01c7a1ec
Dennis Iversen <deiv@aarhus.dk>
2023-06-28 11:01:15 +0200

    clean up tets

--- stadsarkiv_client/endpoints/testing.py
+++ stadsarkiv_client/endpoints/testing.py
@@ -5,7 +5,8 @@ from stadsarkiv_client.core.dynamic_settings import settings
 import json
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core import api
-from stadsarkiv_client.records.record_alter import record_alter
+from stadsarkiv_client.records import record_alter
+from stadsarkiv_client.records.meta_data import get_meta_data
 
 
 log = get_log()
@@ -16,58 +17,3 @@ async def test(request: Request):
     context_variables = {"settings": settings_json, "title": "Test"}
     context = await get_context(request, context_variables)
     return templates.TemplateResponse("testing/test.html", context)
-
-
-def get_section_data(sections, data):
-    section_data = {}
-
-    for section, keys in sections.items():
-        section_values = {}
-        for key in keys:
-            if key in data:
-                section_values[key] = data[key]
-
-        if section_values:
-            section_data[section] = section_values
-
-    return section_data
-
-
-def get_record_and_types(record):
-    record_altered = {}
-    for key, value in record.items():
-        record_item = {}
-        record_item["value"] = value
-        record_item["name"] = key
-
-        try:
-            definition = settings["record_definitions"][key]
-            record_item["type"] = definition["type"]
-        except KeyError:
-            record_item["type"] = "unknown"
-
-        record_altered[key] = record_item
-
-    return record_altered
-
-
-async def test_entitites_macro(request: Request):
-    record_id = request.path_params["record_id"]
-
-    record = await api.proxies_record_get_by_id(record_id)
-    record_altered = record_alter(request, record)
-
-    record_sections = settings["record_sections"]
-    record_and_types = get_record_and_types(record_altered)
-
-    sections = get_section_data(record_sections, record_and_types)
-    context_variables = {
-        "title": record_altered["title"],
-        "record_original": record,
-        "record_and_types": record_and_types,
-        "record_altered": record_altered,
-        "sections": sections,
-    }
-
-    context = await get_context(request, context_variables)
-    return templates.TemplateResponse("records/record.html", context)
