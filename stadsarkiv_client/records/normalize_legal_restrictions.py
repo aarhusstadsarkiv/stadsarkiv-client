fa5bd359cb41c83e5d76f6e5977293225b15d50b
Dennis Iversen <deiv@aarhus.dk>
2023-06-29 13:02:20 +0200

    add legal restrictions to translations

--- stadsarkiv_client/records/normalize_legal_restrictions.py
+++ stadsarkiv_client/records/normalize_legal_restrictions.py
@@ -1,38 +1,19 @@
+from stadsarkiv_client.core.translate import translate
+
+
 def normalize_legal_restrictions(record: dict):
     """Add legal_restrictions_normalized to record"""
 
     text = ""
     legal_id = record["legal_id"]
     if legal_id == 1:
-        text = "Materialet er ikke underlagt andre juridiske begrænsninger af tilgængeligheden."
+        text = translate("legal_restrictions_id_1")
     elif legal_id == 2:
-        text = "Materialet er utilgængeligt ifølge persondatalovgivningen."
+        text = translate("legal_restrictions_id_2")
     elif legal_id == 3:
-        text = "Materialet er utilgængeligt ifølge arkivlovgivningen."
+        text = translate("legal_restrictions_id_3")
     elif legal_id == 4:
-        text = "Materialet er utilgængeligt som følge af særlige juridiske forhold."
+        text = translate("legal_restrictions_id_4")
 
     record["other_legal_restrictions_normalized"] = text
-    return record
-
-
-def _normalize_legal_restrictions(record: dict):
-    legal_id = record["legal_id"]
-    contractual_id = record["contractual_id"]
-    availability_id = record["availability_id"]
-
-    output_text = "Materialet er online her på Aarhusarkivet.dk, men det er det enkelte materiales ophavsretslige status, "
-    output_text += "der fastsætter, hvad et givent materiale videre må bruges til."
-
-    if legal_id > 1 or contractual_id == 1:
-        output_text = "Materialet er utilgængeligt som følge af nævnte juridiske forhold."
-    elif contractual_id == 2:
-        output_text = "Materialet er kun tilgængeligt gennem ansøgning."
-    elif availability_id == 2:
-        output_text = "Materialet skal bestilles hjem til læsesalen, før det kan beses."
-    elif availability_id == 3:
-        output_text = "Materialet er tilgængeligt på læsesalen. Der kræves ikke forudgående bestilling for at se materialet på læsesalen. "
-        output_text += "Man skal blot møde op i åbningstiderne."
-
-    record["other_legal_restrictions_normalized"] = output_text
     return record
