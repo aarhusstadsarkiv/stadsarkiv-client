af7d5da17653a380e173c0fc184a4f9ea5ce7ca1
Dennis Iversen <deiv@aarhus.dk>
2023-07-06 11:24:37 +0200

    update search js module

--- stadsarkiv_client/static/js/search.js
+++ stadsarkiv_client/static/js/search.js
@@ -1,3 +1,6 @@
+import { Events } from '/static/js/events.js';
+import { Flash } from '/static/js/flash.js';
+
 // Function to save the state of the tree to local storage
 function saveTree() {
     const detailsElements = document.querySelectorAll('details');
@@ -30,14 +33,108 @@ function expandTree() {
     }
 }
 
+/**
+ * Check if a string is a valid date
+ */
+function isValidDate(dateString) {
+    // Parse the input string as yyyy-mm-dd
+    var dateParts = dateString.split("-");
+    var year = parseInt(dateParts[0], 10);
+    var month = parseInt(dateParts[1], 10);
+    var day = parseInt(dateParts[2], 10);
+
+    // Create a new Date object using the parsed values
+    var date = new Date(year, month - 1, day);
+
+    // Check if the parsed date is valid
+    return (
+        date.getFullYear() === year &&
+        date.getMonth() === month - 1 &&
+        date.getDate() === day
+    );
+}
+
+/**
+ * Search form
+ * Check if dates are valid and add hidden date inputs to form
+ * formatted to yyyymmdd
+ */
+Events.addEventListenerMultiple('#search-date', 'submit', function (e) {
+    e.preventDefault();
+    let fromYear = document.getElementById('from-year').value;
+    let fromMonth = document.getElementById('from-month').value || '01';
+    let fromDay = document.getElementById('from-day').value || '01';
+
+    // let currentYear = new Date().getFullYear();
+
+    let toYear = document.getElementById('to-year').value;
+    let toMonth = document.getElementById('to-month').value || '12';
+    let toDay = document.getElementById('to-day').value || '31';
+
+    if (!fromYear && !toYear) {
+        Flash.setMessage('Du skal indtaste en dato mellem 1200 og aktuel data', 'error');
+        return;
+    }
+
+    // One date must be valid now
+    let fromDate = `${fromYear}-${fromMonth}-${fromDay}`;
+    let toDate = `${toYear}-${toMonth}-${toDay}`;
+
+    if (isValidDate(fromDate)) {
+        // add hidden input to form
+        let input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'date_from';
+        input.value = `${fromYear}${fromMonth}${fromDay}`;
+        document.getElementById('search-date').appendChild(input);
+    }
+
+    if (isValidDate(toDate)) {
+        // add hidden input to form
+        let input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'date_to';
+        input.value = `${toYear}${toMonth}${toDay}`;;
+        document.getElementById('search-date').appendChild(input);
+    }
+
+    // Set disabled on existing inputs to prevent them from being submitted
+    document.getElementById('from-year').disabled = true;
+    document.getElementById('from-month').disabled = true;
+    document.getElementById('from-day').disabled = true;
+    document.getElementById('to-year').disabled = true;
+    document.getElementById('to-month').disabled = true;
+    document.getElementById('to-day').disabled = true;
+
+    // Submit new form to current url with new hidden inputs
+    document.getElementById('search-date').submit();
+
+});
+
+/**
+ * Ensure only numbers can be entered in the date fields
+ */
+Events.addEventListenerMultiple('#search-date > input', 'input', function (e) {
+    let input = e.target;
+    input.value = input.value.replace(/\D/g, '');
+});
+
+/**
+ * Expand tree based on saved state
+ */
 document.addEventListener('DOMContentLoaded', () => {
     try {
         expandTree();
         window.addEventListener('beforeunload', saveTree);
-    
+
     } catch (error) {
         // unset local storage if it fails. The tree may be updated and the saved state may be invalid
         localStorage.removeItem('treeState');
         console.log(error);
     }
 });
+
+
+let Search = {};
+
+export { Search }
