4bfa0d5e06d5c8d28d273b146c885d49c7b4b805
Dennis Iversen <deiv@aarhus.dk>
2023-08-04 13:48:02 +0200

    shift burger menu icon. No need for DOMContentLoaded event

--- stadsarkiv_client/static/js/main-events.js
+++ stadsarkiv_client/static/js/main-events.js
@@ -1,17 +1,19 @@
 /**
  * Burger menu
  */
-document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('menu-hamburger').addEventListener('click', function(event) {
-        event.preventDefault();
-        const menu = document.querySelector('.main-menu');
-        if (menu.style.display === "none" || menu.style.display === "") {
-            menu.style.display = "block";
-        } else {
-            menu.style.display = "none";
-        }
-    });
+
+document.getElementById('menu-hamburger').addEventListener('click', function (event) {
+    event.preventDefault();
+    const menu = document.querySelector('.main-menu');
+    const menuText = document.querySelector('#menu-hamburger > .material-symbols-outlined');
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+        menuText.textContent = "menu_open"; 
+    } else {
+        menu.style.display = "none";
+        menuText.textContent = "menu"; 
+    }
 });
 
-export {}
+export { }
