781bda0776cee15cc7763a48bbc9bfa55713efbe
Dennis Iversen <deiv@aarhus.dk>
2023-08-04 13:25:44 +0200

    burger menu and sticky menu

--- stadsarkiv_client/static/js/main-events.js
+++ stadsarkiv_client/static/js/main-events.js
@@ -1,5 +1,17 @@
-import { Flash } from "./flash.js";
+/**
+ * Burger menu
+ */
+document.addEventListener('DOMContentLoaded', function() {
 
-console.log('main-events loaded')
+    document.getElementById('menu-hamburger').addEventListener('click', function(event) {
+        event.preventDefault();
+        const menu = document.querySelector('.main-menu');
+        if (menu.style.display === "none" || menu.style.display === "") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    });
+});
 
 export {}
