a56a14b6dc5a8d4b9430423c40627a0c71aa72fb
Dennis Iversen <deiv@aarhus.dk>
2023-08-10 13:25:14 +0200

    test query params to endpoint

--- stadsarkiv_client/endpoints/testing.py
+++ stadsarkiv_client/endpoints/testing.py
@@ -1,16 +1,15 @@
 from starlette.requests import Request
-from stadsarkiv_client.core.templates import templates
-from stadsarkiv_client.core.context import get_context
-from stadsarkiv_client.core.dynamic_settings import settings
-import json
 from stadsarkiv_client.core.logging import get_log
-
+from stadsarkiv_client.core import api
+from starlette.responses import JSONResponse
 
 log = get_log()
 
 
 async def test(request: Request):
-    settings_json = json.dumps(settings, indent=4, ensure_ascii=False)
-    context_variables = {"settings": settings_json, "title": "Test"}
-    context = await get_context(request, context_variables)
-    return templates.TemplateResponse("testing/test.html", context)
+    # settings_json = json.dumps(settings, indent=4, ensure_ascii=False)
+    # context_variables = {"settings": settings_json, "title": "Test"}
+    # context = await get_context(request, context_variables)
+    # return templates.TemplateResponse("testing/test.html", context)
+    results = await api.proxies_records_from_list([("size", "10"), ("q", "hvilsager"), ("view", "ids")])
+    return JSONResponse(results)
