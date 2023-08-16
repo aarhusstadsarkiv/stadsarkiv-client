5627841a0cda54ddedbbc78345cae34eba7c400f
Dennis Iversen <deiv@aarhus.dk>
2023-08-16 15:07:46 +0200

    add sentry snippet

--- stadsarkiv_client/app.py
+++ stadsarkiv_client/app.py
@@ -6,14 +6,27 @@ import os
 import json
 from stadsarkiv_client.core.dynamic_settings import settings
 from stadsarkiv_client.core.logging import get_log
+import sentry_sdk
 
 
 log = get_log()
 
-
 log.debug("Environment: " + str(os.getenv("ENVIRONMENT")))
 log.debug(json.dumps(settings, sort_keys=True, indent=4, ensure_ascii=False))
 
+sentry_dns = os.getenv('SENTRY_DNS')
+
+if sentry_dns:
+
+    log.info("Sentry DNS: " + str(sentry_dns))
+    sentry_sdk.init(
+        dsn=sentry_dns,
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
+        traces_sample_rate=1.0,
+    )
 
 app = Starlette(
     debug=settings["debug"],  # type: ignore
