a277ff3313bf8ab27962ec8278e1550b417e14b1
Dennis Iversen <deiv@aarhus.dk>
2023-08-17 14:58:15 +0200

    add overlay to image

--- stadsarkiv_client/static/js/record.js
+++ stadsarkiv_client/static/js/record.js
@@ -1,4 +1,7 @@
-document.addEventListener('keydown', function(e) {
+/**
+ * Next prev shortcuts for search results displayed on record page
+ */
+document.addEventListener('keydown', function (e) {
     if (e.key === 'ArrowLeft') {
         const prevElem = document.querySelector('.prev');
         if (prevElem) {
@@ -14,4 +17,43 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-export {  }
+/**
+ * Simple overlay for record image
+ */
+
+let overlayHistoryModified = false;
+
+const recordImage = document.querySelector('.record-image');
+if (recordImage) {
+    recordImage.addEventListener('click', function () {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'block';
+
+        // Only modify history if it hasn't been modified before by the overlay
+        // Add to history so that the user can use the back button to close the overlay
+        if (!overlayHistoryModified) {
+            history.pushState({ overlayOpened: true }, null, null);
+            overlayHistoryModified = true;
+        }
+    });
+}
+
+const overlay = document.getElementById('overlay');
+if (overlay) {
+    overlay.addEventListener('click', function () {
+        this.style.display = 'none';
+        overlayHistoryModified = false; // Reset the flag
+    });
+}
+
+window.addEventListener('popstate', function (event) {
+    const overlay = document.getElementById('overlay');
+
+    // If the overlay is displayed, hide it
+    if (overlay.style.display === 'block') {
+        overlay.style.display = 'none';
+        overlayHistoryModified = false; // Reset the flag
+    }
+});
+
+export { }
