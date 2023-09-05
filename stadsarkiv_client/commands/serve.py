2fa2fb601807e619050e20bc512a0aac5fdbd228
Dennis Iversen <deiv@aarhus.dk>
2023-09-05 13:17:21 +0200

    add host as option

--- stadsarkiv_client/commands/serve.py
+++ stadsarkiv_client/commands/serve.py
@@ -6,5 +6,6 @@ import uvicorn
 @click.option("--reload", default=True, help="Reload uvicorn on changes.")
 @click.option("--port", default=5555, help="Server port.")
 @click.option("--workers", default=1, help="Number of workers.")
-def run(reload: bool, port: int, workers: int):
-    uvicorn.run("stadsarkiv_client.app:app", reload=reload, port=port, workers=workers, log_level="info")
+@click.option("--host", default="0.0.0.0", help="Server host.")
+def run(reload: bool, port: int, workers: int, host: str):
+    uvicorn.run("stadsarkiv_client.app:app", reload=reload, port=port, workers=workers, host=host, log_level="info")
