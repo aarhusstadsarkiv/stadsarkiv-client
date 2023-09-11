4b8b6b0fd1e2a62eb615222be728953f3cf98c26
Dennis Iversen <deiv@aarhus.dk>
2023-09-11 13:57:05 +0200

    update script

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
-# check if first argument is set 
-if [ -z "$1" ]
-  then
-    echo "No argument supplied"
+# Exit on any error
+# set -e
+
+# check if tag is provided 
+if [ -z "$1" ]; then
+    echo "No tag argument supplied"
     exit 1
 fi
 
-# tag is the first argument
-TAG=$1
+tag=$1
 
+# Fetch latest changes without modifying the working tree
+git fetch
+
+# Check if tag exists in the fetched data
+if ! git rev-parse $tag >/dev/null 2>&1; then
+    echo "Tag $tag does not exist in the repo"
+    exit 1
+fi
+
+# Stop the service
 sudo systemctl stop stadsarkiv-client.service
 
+# Upgrade the repo
+git checkout main
 git pull
-git checkout $TAG
-./venv/bin/pip install -r requirements.txt
+git checkout $tag
 
+# Activate virtual environment and install requirements
+source ./venv/bin/activate
+pip install -r requirements.txt
+
+# Start the service
 sudo systemctl start stadsarkiv-client.service
