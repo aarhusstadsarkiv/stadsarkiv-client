9d055cbebe4d5c93e31fa6bbf7f0b484250c7fb8
Dennis Iversen <deiv@aarhus.dk>
2023-09-12 11:12:57 +0200

    better exception handlers

--- stadsarkiv_client/core/exception_handlers.py
+++ stadsarkiv_client/core/exception_handlers.py
@@ -14,18 +14,25 @@ log = get_log()
 
 async def not_found(request: Request, exc: HTTPException):
     context_values = {"title": translate("404 Not Found")}
+
+    # No need to log full exception. It's a 404
+    log.error(f"404 Not Found: {request.url}")
     context = await get_context(request, context_values=context_values)
     return templates.TemplateResponse("errors/default.html", context, status_code=404)
 
 
 async def server_error(request: Request, exc: HTTPException):
     context_values = {"title": translate("500 Server Error")}
+
+    log.exception(f"500 Error: {request.url}", exc_info=exc)
     context = await get_context(request, context_values=context_values)
     return templates.TemplateResponse("errors/default.html", context, status_code=500)
 
 
 async def forbidden_error(request: Request, exc: HTTPException):
     context_values = {"title": translate("403 Forbidden Error")}
+
+    log.error(f"403 Forbidden: {request.url}", exc_info=exc)
     context = await get_context(request, context_values=context_values)
     return templates.TemplateResponse("errors/default.html", context, status_code=403)
 
