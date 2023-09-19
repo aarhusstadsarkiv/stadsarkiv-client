f84eb4dbbf1749d7630e07abad79d73731f04b2d
Dennis Iversen <deiv@aarhus.dk>
2023-09-19 14:56:59 +0200

    clean up example hooks

--- example-config/hooks.py
+++ example-config/hooks.py
@@ -1,40 +1,17 @@
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core.hooks_spec import HooksSpec
+from stadsarkiv_client.records.record_utils import is_curator, is_collection
 
 
 log = get_log()
 
 
-def _is_teater_arkiv(record_dict: dict):
-
-    # try and get first list item in curators
-    try:
-        curator = record_dict["curators"][0]
-    except KeyError:
-        return False
-
-    if curator["id"] == 4:
-        return True
-
-    return False
-
-
 class Hooks(HooksSpec):
     def alter_context(self, context: dict) -> None:
+        """
+        Alter the context dictionary. Before the context is returned to the template.
+        """
         pass
-        # context["title"] = context["title"] + " [modified by plugin]"
-
-    def alter_search_query(self, query_params: list) -> list:
-        """
-        Alter the search query params. Before the search is executed.
-        This example removes all curators from the query params and adds Aarhus Teater as curator (4).
-        """
-
-        # Remove all curators from the query params
-        query_params = [(key, value) for key, value in query_params if key != "curators"]
-        query_params.append(("curators", "4"))
-
-        return query_params
 
     def alter_query_params_before_search(self, query_params: list) -> list:
         """
@@ -42,7 +19,7 @@ class Hooks(HooksSpec):
         This example removes all curators from the query params and adds Aarhus Teater as curator (4).
         """
 
-        # Remove all curators from the query params
+        # Remove all curators from the query params and add curator (4)
         query_params = [(key, value) for key, value in query_params if key != "curators"]
         query_params.append(("curators", "4"))
 
@@ -55,23 +32,33 @@ class Hooks(HooksSpec):
         query_params = [(key, value) for key, value in query_params if key != "curators"]
         return query_params
 
-    # def get_record_title(self, title: str, record_dict: dict) -> str:
-    #     return title
-    #
-    # curator
+    def alter_record(self, record_dict: dict) -> None:
+        """
+        Alter the record dictionary. Before the record is returned to the template.
+        """
+        if is_collection(record_dict, 1):
+
+            meta_title = f"[{record_dict['summary'][:60]} ... ]"
+            record_dict["meta_title"] = meta_title
+            record_dict["record_type"] = "sejrs_sedler"
+
+        if is_curator(record_dict, 4):
+            if record_dict.get("summary"):
+                title = record_dict["summary"]
+                record_dict["title"] = f"[{title}]"
+                record_dict["meta_title"] = f"[{title}"
+
     def get_record_title(self, title: str, record_dict: dict) -> str:
         """
         Alter the record title. Before the title is returned to the template.
         """
-
-        if _is_teater_arkiv(record_dict):
-            summary = record_dict.get("summary", "")
-            return summary
-
-        # log.debug("title", title)
-
         return title
 
-    def get_record_meta_title(self, title: str) -> str:
-        title = f"{title} | AarhusArkivet"
-        return title
+    # def get_record_meta_title(self, meta_title: str, record_dict: dict) -> str:
+
+    #     if is_collection(record_dict, 1):
+    #         if record_dict.get("summary"):
+    #             meta_title = f"[{record_dict['summary'][:60]} ... ]"
+
+    #     meta_title = f"{meta_title} | AarhusArkivet"
+    #     return meta_title
