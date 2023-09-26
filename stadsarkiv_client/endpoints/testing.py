8868961f47bfa640939ae64b230178365046e268
Dennis Iversen <deiv@aarhus.dk>
2023-09-26 09:38:02 +0200

    change testing

--- stadsarkiv_client/endpoints/testing.py
+++ stadsarkiv_client/endpoints/testing.py
@@ -6,18 +6,19 @@ Only enabled in development mode
 from starlette.requests import Request
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core import api
-from starlette.responses import PlainTextResponse
+from stadsarkiv_client.core.context import get_context
+from starlette.responses import HTMLResponse, PlainTextResponse
+from stadsarkiv_client.core.templates import templates
 import json
 
 log = get_log()
 
 
 async def test(request: Request):
-    # settings_json = json.dumps(settings, indent=4, ensure_ascii=False)
-    # context_variables = {"settings": settings_json, "title": "Test"}
-    # context = await get_context(request, context_variables)
-    # return templates.TemplateResponse("testing/test.html", context)
-    # results = await api.proxies_records_from_list([("size", "10"), ("q", "hvilsager"), ("view", "ids")])
+    context_values = {"title": "TEST"}
+    context = await get_context(request, context_values=context_values)
+
+    return templates.TemplateResponse("testing/test.html", context)
 
     results = await api.proxies_entity_by_type("people", "121365")
     json_str = json.dumps(results, indent=4, ensure_ascii=False)
