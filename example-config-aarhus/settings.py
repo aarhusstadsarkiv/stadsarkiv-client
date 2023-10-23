676ace88118332d60d168431504e27c63ed1d833
Dennis Iversen <deiv@aarhus.dk>
2023-10-23 15:07:39 +0200

    add example settings

---
+++ example-config-aarhus/settings.py
@@ -0,0 +1,45 @@
+import typing
+
+
+settings: dict[str, typing.Any] = {
+    "language": "da",
+    "log_handlers": ["stream"],  # [ "stream", "file"]
+    #
+    # cookie settings
+    #
+    "cookie": {
+        "name": "session",
+        "lifetime": 3600,  # seconds
+        "httponly": True,
+        "secure": True,
+        "samesite": "lax",
+    },
+    "api_base_url": "https://dev.openaws.dk/v1",
+    #
+    # Main menu containing built-in endpoints, but you may remove these and generate your own menu.
+    # You may also add other "menus", e.g. "footer_items" or something similar.
+    #
+    "main_menu": [
+        {"name": "login", "title": "Log ind"},
+        {"name": "forgot_password", "title": "Glemt password"},
+        {"name": "logout", "title": "Log ud"},
+        {"name": "register", "title": "Ny bruger"},
+        {"name": "profile", "title": "Profil"},
+        {"name": "schemas", "title": "Schemas"},
+        {"name": "entities", "title": "Entities"},
+        {"name": "records_search", "title": "Søg"},
+    ],
+    #
+    # Custom pages
+    #
+    # "name" is the route name. Title is the page title.
+    # "template" if the page you will use. It is also the content of the page.
+    # "url" is the path to the page
+    #
+    # You can then make another menu which contains some of these pages, e.g. a "footer_items" entry
+    # or something similar.
+    "pages": [
+        {"name": "home", "title": "Hjem", "template": "pages/home.html", "url": "/"},
+    ],
+    "search_base_url": "/search",
+}
