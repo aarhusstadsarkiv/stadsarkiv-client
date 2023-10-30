4e115637e91c99d22cde8c5b1f6e5076c1cfc519
Dennis Iversen <deiv@aarhus.dk>
2023-10-30 13:50:50 +0100

    use only args in disk_cache

--- stadsarkiv_client/core/decorators.py
+++ stadsarkiv_client/core/decorators.py
@@ -62,7 +62,7 @@ if not os.path.exists(CACHE_DIR):
     os.makedirs(CACHE_DIR)
 
 
-def disk_cache(ttl: int, use_args: list = [], use_kwargs: list = []):
+def disk_cache(ttl: int, use_args: list = []):
     """
     Cache the result of a function to disk.
     """
@@ -70,18 +70,18 @@ def disk_cache(ttl: int, use_args: list = [], use_kwargs: list = []):
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
-            relevant_kwargs = {k: v for k, v in kwargs.items() if k in use_kwargs}
             relevant_args = [arg for i, arg in enumerate(args) if i in use_args]
 
             # Check if relevant_args and relevant_kwargs are not empty. Throw error if they are.
-            if not relevant_kwargs and not relevant_args:
+            if not relevant_args:
                 raise Exception("You can not use disk_cache on a function with no arguments or keyword arguments.")
 
             # Generate a cache key based on the function's name and its arguments.
-            cache_key = f"{func.__name__}_{str(relevant_args)}_{str(relevant_kwargs)}"
+            cache_key = f"{func.__name__}_{str(relevant_args)}"
             cache_file = os.path.join(CACHE_DIR, f"cache_{hash(cache_key)}.pkl")
 
             if os.path.exists(cache_file):
+                log.debug(f"Cache file exists: {cache_file}")
                 with open(cache_file, "rb") as f:
                     cache_data = pickle.load(f)
                     if time.time() - cache_data["timestamp"] <= ttl:
