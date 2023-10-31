f8ea68d7728aac308d59b4d9e04f90a2efa443f7
Dennis Iversen <deiv@aarhus.dk>
2023-10-31 12:37:28 +0100

    css scroll-margin-top. Add json-editor method for parsing errors

--- stadsarkiv_client/static/js/jsoneditor_utils.js
+++ stadsarkiv_client/static/js/jsoneditor_utils.js
@@ -1,3 +1,5 @@
+import { Flash } from "/static/js/flash.js";
+
 async function getTranslation(lang) {
 
     const url = '/static/json/jsoneditor_' + lang + '.json';
@@ -9,4 +11,33 @@ async function getTranslation(lang) {
     return translation
 }
 
-export {getTranslation}
+function parseErrors(schema, errors) {
+    errors.forEach(error => {
+
+        // Get field title from json schema
+        console.log(error)
+        
+        // Remove root. from error.path
+        const field = error.path.split('.').slice(1).join('.')
+        
+        // Split field into parts
+        const fieldParts = field.split('.')
+
+        // Go into schema.data.properties and find the correct field
+        let fieldSchema = schema.data
+
+        fieldParts.forEach(field_part => {
+            fieldSchema = fieldSchema.properties[field_part]
+        });
+
+        const fieldTitle = fieldSchema.title || error.path
+        const message = "Validerings fejl i feltet " + fieldTitle + ": " + error.message;
+
+        const element = document.querySelector(`[data-schemapath="root.${field}"]`)
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+
+        Flash.setMessage(message, 'error')
+    });
+}
+
+export {getTranslation, parseErrors}
