8bc80f796fb61bbf14ae153b7a00e12fc15cb3e9
Dennis Iversen <deiv@aarhus.dk>
2023-11-09 10:42:50 +0100

    clean up test page

--- stadsarkiv_client/endpoints/testing.py
+++ stadsarkiv_client/endpoints/testing.py
@@ -5,11 +5,9 @@ Only enabled in development mode
 
 from starlette.requests import Request
 from stadsarkiv_client.core.logging import get_log
-from stadsarkiv_client.core import api
 from stadsarkiv_client.core.context import get_context
-from starlette.responses import HTMLResponse, PlainTextResponse
 from stadsarkiv_client.core.templates import templates
-import json
+
 
 log = get_log()
 
@@ -19,7 +17,3 @@ async def test(request: Request):
     context = await get_context(request, context_values=context_values)
 
     return templates.TemplateResponse("testing/test.html", context)
-
-    results = await api.proxies_get_resource("people", "121365")
-    json_str = json.dumps(results, indent=4, ensure_ascii=False)
-    return PlainTextResponse(json_str)
