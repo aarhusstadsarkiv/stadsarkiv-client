48bc1385b391cbc448eb6c550a5f3f14f19e7340
Dennis Iversen <deiv@aarhus.dk>
2023-11-24 14:55:32 +0100

    add onBlur to remove suggestions. Clean up

--- stadsarkiv_client/static/js/auto-complete.js
+++ stadsarkiv_client/static/js/auto-complete.js
@@ -1,12 +1,12 @@
 class AutoComplete {
     constructor(options) {
-        
+
         // Required parameters
         this.autocompleteElem = options.autocompleteElem;
         this.suggestionsElem = options.suggestionsElem;
         this.renderFunction = options.renderFunction;
         this.endpoint = options.endpoint;
-        
+
         // Optional parameters
         this.debounceTimer = options.debounceTimer || 500;
         this.lastInputValue = options.lastInputValue || '';
@@ -20,12 +20,25 @@ class AutoComplete {
         // Bind methods
         this.autocompleteElem.addEventListener('input', this.onInput.bind(this));
         this.autocompleteElem.addEventListener('keydown', this.onKeyDown.bind(this));
-        
-        // Hide suggestions on resize
-        this.hideSuggestions();
-        
+
+        // Bind the onBlur method
+        this.onBlur = this.onBlur.bind(this);
+
+        // Add the blur event listener
+        this.autocompleteElem.addEventListener('blur', this.onBlur);
+
         // Hide suggestions on resize
         window.addEventListener('resize', this.hideSuggestions.bind(this));
+
+        // Hide suggestions on load
+        this.hideSuggestions();
+    }
+
+    onBlur() {
+        // Set a short timeout to allow clicks on suggestions to register
+        setTimeout(() => {
+            this.hideSuggestions();
+        }, this.debounceTimer);
     }
 
     hideSuggestions() {
@@ -39,7 +52,7 @@ class AutoComplete {
         clearTimeout(this.timeoutID);
         const inputLength = this.autocompleteElem.value.length;
         const inputValue = event.target.value;
-        
+
         if (inputLength < this.minInputLength) {
             this.hideSuggestions();
             return;
@@ -52,23 +65,7 @@ class AutoComplete {
         this.timeoutID = setTimeout(() => {
             fetch(`${this.endpoint}${inputValue}`)
                 .then(response => response.json())
-                .then( data => {
-
-                    // FOR TESTING:
-                    // 
-                    // Just ontil we get some real data
-                    // We need to randomize the data and remove some items
-                    // to simulate a real search
-
-                    // Sort the data randomly
-                    // data.sort(() => Math.random() - 0.5);
-                    
-                    // // Remove between 1 and 10 items (random)
-                    // const random = Math.floor(Math.random() * 10) + 1;
-                    
-                    // // Remove the random items
-                    // data.splice(0, random);
-                    
+                .then(data => {
                     this.updateSuggestions(data)
                 });
         }, this.debounceTimer);
@@ -81,13 +78,13 @@ class AutoComplete {
 
         const items = this.suggestionsElem.querySelectorAll(`.${this.suggestionItemClass}`);
         if (items.length === 0 && event.key !== 'Escape') return;
-    
+
         if (event.key === 'Escape') {
             event.preventDefault();
             this.hideSuggestions();
             return;
         }
-    
+
         let currentIndex = -1;
         items.forEach((item, index) => {
             if (item.classList.contains(this.suggestionFocusClass)) {
@@ -95,36 +92,37 @@ class AutoComplete {
                 item.classList.remove(this.suggestionFocusClass);
             }
         });
-    
+
         if (event.key === 'ArrowDown') {
             currentIndex = currentIndex < items.length - 1 ? currentIndex + 1 : 0;
         } else if (event.key === 'ArrowUp') {
             currentIndex = currentIndex > 0 ? currentIndex - 1 : items.length - 1;
         } else if (event.key === 'Enter' && currentIndex !== -1) {
+            event.preventDefault();
             const focusedItem = items[currentIndex];
             const url = focusedItem.getAttribute('data-url');
             if (url) {
                 window.location.href = url;
+                return;
             }
         } else {
             return;
         }
-    
+
         const newItem = items[currentIndex];
         newItem.classList.add(this.suggestionFocusClass);
-    
+
         // Scroll the new item into view
         newItem.scrollIntoView({ block: 'nearest', inline: 'start' });
-    
+
         // Focus on the input again
         this.autocompleteElem.focus();
     }
-    
+
     updateSuggestions(data) {
 
         const inputRect = this.autocompleteElem.getBoundingClientRect();
 
-        // set display to flex
         this.suggestionsElem.style.display = 'block';
         this.suggestionsElem.style.top = `${inputRect.bottom - 80}px`;
         this.suggestionsElem.style.left = `${inputRect.left}px`;
