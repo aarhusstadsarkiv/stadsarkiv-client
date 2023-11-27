bcb6441da4c2e5366e4433f5ee9a1464ff338cfd
Dennis Iversen <deiv@aarhus.dk>
2023-11-27 13:06:16 +0100

    refactor. add def 'get_local_hooks'

--- stadsarkiv_client/core/hooks.py
+++ stadsarkiv_client/core/hooks.py
@@ -6,11 +6,17 @@ import importlib
 
 log = get_log()
 
-# Output the hooks that are loaded on startup
-try:
+
+def get_local_hooks():
     module_name = get_local_config_dir() + ".hooks"
     submodule = importlib.import_module(module_name)
     HooksLocal = getattr(submodule, "Hooks")
+    return HooksLocal
+
+
+# Output the hooks that are loaded on startup
+try:
+    get_local_hooks()
     log.debug(f"Loaded local hooks: {get_local_config_dir('hooks.py')}")
 except ImportError:
     log.debug(f"Local hooks NOT loaded: {get_local_config_dir('hooks.py')}")
@@ -21,9 +27,7 @@ def get_hooks(request: Request) -> HooksSpec:
     Get local hooks if they exist, otherwise get the default hooks
     """
     try:
-        module_name = get_local_config_dir() + ".hooks"
-        submodule = importlib.import_module(module_name)
-        HooksLocal = getattr(submodule, "Hooks")
+        HooksLocal = get_local_hooks()
         hooks_local = HooksLocal(request)
         return hooks_local
     except ImportError:
