e13ae9c356866ea6e3fca8a7441d7e38c18e3f07
Dennis Iversen <deiv@aarhus.dk>
2023-12-04 10:35:08 +0100

    refactor. normalize_fields > normalize_resource. Same pattern as normalize_record

--- stadsarkiv_client/resources/resource_alter.py
+++ stadsarkiv_client/resources/resource_alter.py
@@ -4,7 +4,7 @@ e.g. set sources_normalized, set outer years, etc.
 """
 
 from stadsarkiv_client.core.logging import get_log
-from stadsarkiv_client.resources import normalize_fields
+from stadsarkiv_client.resources import normalize_resource
 
 
 log = get_log()
@@ -15,13 +15,13 @@ def resource_alter(resource: dict):
 
     relations = resource.get("relations")
 
-    resource = normalize_fields.set_created_decommissioned(resource)
-    resource = normalize_fields.set_collectors_link_list(resource, schema)
-    resource = normalize_fields.set_creators_link_list(resource, schema)
-    resource = normalize_fields.set_sources_normalized(resource)
-    resource = normalize_fields.set_outer_years(resource)
-    resource = normalize_fields.set_latitude_longitude(resource)
-    resource = normalize_fields.get_resource_and_types(resource)
+    resource = normalize_resource.set_created_decommissioned(resource)
+    resource = normalize_resource.set_collectors_link_list(resource, schema)
+    resource = normalize_resource.set_creators_link_list(resource, schema)
+    resource = normalize_resource.set_sources_normalized(resource)
+    resource = normalize_resource.set_outer_years(resource)
+    resource = normalize_resource.set_latitude_longitude(resource)
+    resource = normalize_resource.get_resource_and_types(resource)
 
     # These are either string_list or link_list.
     string_list_or_link_list = [
@@ -31,7 +31,7 @@ def resource_alter(resource: dict):
 
     for elem in string_list_or_link_list:
         if elem in resource:
-            resource[elem] = normalize_fields.get_string_or_link_list(elem, resource[elem])
+            resource[elem] = normalize_resource.get_string_or_link_list(elem, resource[elem])
 
     resource["relations"] = relations
     return resource
