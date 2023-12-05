f82eead8845750c7803a1ff108890c3f33cb0a8a
Dennis Iversen <deiv@aarhus.dk>
2023-12-05 13:32:03 +0100

    test collection/2 to see if html is as expected in collection

---
+++ tests/default/html_test.py
@@ -0,0 +1,30 @@
+"""
+Test of some resources and records
+"""
+
+from stadsarkiv_client.app import app
+from stadsarkiv_client.core.logging import get_log
+from starlette.testclient import TestClient
+import unittest
+from bs4 import BeautifulSoup
+
+log = get_log()
+
+
+class TestPages(unittest.TestCase):
+    def test_collections(self):
+        client = TestClient(app)
+        response = client.get("/collections/2")
+
+        # check the response html and test if it contains a "div" with the class "slideshow-container"
+        soup = BeautifulSoup(response.content, "html.parser")
+        slideshow_container = soup.find("div", {"class": "slideshow-container"})
+        self.assertIsNotNone(slideshow_container)
+
+        # check the response html and test if it contains a "h3" with the text "Arkivserier"
+        h3Arkiv = soup.find("h3", string="Arkivserier")
+        self.assertIsNotNone(h3Arkiv)
+
+        # check the response html and test if it contains a "h3" with the text "Arkivserier"
+        h3Tags = soup.find("h3", string="Samlingstags")
+        self.assertIsNotNone(h3Tags)
