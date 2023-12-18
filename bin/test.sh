09fb66c117540fe0adc2b3ec5065afc4249a15de
Dennis Iversen <deiv@aarhus.dk>
2023-12-18 13:04:01 +0100

    alter config dir names in tests

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # This script runs tests
-./venv/bin/python -m unittest tests/default/*.py
+./venv/bin/python -m unittest tests/config-default/*.py
 
 # Test teater client
 export CONFIG_DIR="example-config-teater"
-./venv/bin/python -m unittest tests/client/*.py
+./venv/bin/python -m unittest tests/config-teater/*.py
