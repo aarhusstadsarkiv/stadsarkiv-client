3b54889db6e1ee27246b76faf888672e9b10df39
Dennis Iversen <deiv@aarhus.dk>
2023-12-18 13:14:48 +0100

    add aarhus test

--- bin/test.sh
+++ bin/test.sh
@@ -6,3 +6,7 @@
 # Test teater client
 export CONFIG_DIR="example-config-teater"
 ./venv/bin/python -m unittest tests/config-teater/*.py
+
+# Test aarhus
+export CONFIG_DIR="example-config-aarhus"
+./venv/bin/python -m unittest tests/config-aarhus/*.py
