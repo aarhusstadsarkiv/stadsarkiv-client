b8510d741495135a1b4124654ad1e55d0534393c
Dennis Iversen <deiv@aarhus.dk>
2023-12-19 14:59:24 +0100

    allow for multiple slideshows on same page

--- stadsarkiv_client/static/js/slideshow.js
+++ stadsarkiv_client/static/js/slideshow.js
@@ -1,78 +1,92 @@
-// Display .prev .next .slide-counter
-document.querySelector(".prev").style.display = "block";
-document.querySelector(".next").style.display = "block";
-document.querySelector(".slide-counter").style.display = "block";
+class Slideshow {
+    constructor(slideshowContainer) {
+        this.slideshow = slideshowContainer;
+        this.currentSlideIndex = 0;
+        this.autoSlideShow = null;
+        this.SECONDS = 5;
+        this.initSlideshow();
+        this.startAutoSlideShow();
+    }
 
-let currentSlideIndex = 0;
+    initSlideshow() {
+        this.slideshow.querySelector(".prev").style.display = "block";
+        this.slideshow.querySelector(".next").style.display = "block";
+        this.slideshow.querySelector(".slide-counter").style.display = "block";
 
-showSlide(currentSlideIndex);
+        this.showSlide(this.currentSlideIndex);
+        this.addEventListeners();
+    }
 
-function moveSlide(n) {
-    showSlide(currentSlideIndex += n);
+    moveSlide(n) {
+        this.showSlide(this.currentSlideIndex += n);
+    }
+
+    showSlide(n) {
+        let slides = this.slideshow.querySelectorAll(".slide");
+        let currentSlideElem = this.slideshow.querySelector(".current-slide");
+        let totalSlidesElem = this.slideshow.querySelector(".total-slides");
+
+        if (n >= slides.length) {
+            this.currentSlideIndex = 0;
+        }
+
+        if (n < 0) {
+            this.currentSlideIndex = slides.length - 1;
+        }
+
+        for (let slide of slides) {
+            slide.style.display = "none";
+        }
+
+        slides[this.currentSlideIndex].style.display = "block";
+        currentSlideElem.textContent = this.currentSlideIndex + 1;
+        totalSlidesElem.textContent = slides.length;
+    }
+
+    startAutoSlideShow() {
+        this.autoSlideShow = setInterval(() => {
+            this.moveSlide(1);
+        }, 1000 * this.SECONDS);
+    }
+
+    stopAutoSlideShow() {
+        clearInterval(this.autoSlideShow);
+    }
+
+    addEventListeners() {
+        this.slideshow.querySelector(".prev").addEventListener("click", (event) => {
+            this.stopAutoSlideShow();
+            event.preventDefault();
+            this.moveSlide(-1);
+        });
+
+        this.slideshow.querySelector(".next").addEventListener("click", (event) => {
+            this.stopAutoSlideShow();
+            event.preventDefault();
+            this.moveSlide(1);
+        });
+
+        // document.addEventListener("keydown", (event) => {
+        //     if (event.key === "ArrowLeft") {
+        //         this.stopAutoSlideShow();
+        //         event.preventDefault();
+        //         this.moveSlide(-1);
+        //     } else if (event.key === "ArrowRight") {
+        //         this.stopAutoSlideShow();
+        //         event.preventDefault();
+        //         this.moveSlide(1);
+        //     }
+        // });
+    }
 }
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    let currentSlideElem = document.querySelector(".current-slide");
-    let totalSlidesElem = document.querySelector(".total-slides");
-
-    // wrap to the start
-    if (n >= slides.length) {
-        currentSlideIndex = 0;
-    }
-
-    // wrap to the end
-    if (n < 0) {
-        currentSlideIndex = slides.length - 1;
-    }
-
-    // Hide all slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Display the current slide
-    slides[currentSlideIndex].style.display = "block";
-
-    // Update the slide counter
-    // +1 since we want to display from 1..n instead of 0..n-1
-    currentSlideElem.textContent = currentSlideIndex + 1;
-    totalSlidesElem.textContent = slides.length;
+// Usage:
+// const slideshows = document.querySelectorAll(".slideshow-container");
+function initSlideshows(slideshows) {
+    slideshows.forEach(function(slideshow) {
+        new Slideshow(slideshow);
+    })
 }
 
-// Add auto slide show
-const SECONDS = 15;
-let autoSlideShow = setInterval(function () {
-    moveSlide(1);
-}, 1000 * SECONDS);
 
-// Click events
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-
-prev.addEventListener("click", function (event) {
-    clearInterval(autoSlideShow);
-    event.preventDefault();
-    moveSlide(-1);
-});
-
-next.addEventListener("click", function (event) {
-    clearInterval(autoSlideShow);
-    event.preventDefault();
-    moveSlide(1);
-});
-
-// Keyboard events
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-        clearInterval(autoSlideShow);
-        event.preventDefault();
-        moveSlide(-1);
-    } else if (event.key === "ArrowRight") {
-        clearInterval(autoSlideShow);
-        event.preventDefault();
-        moveSlide(1);
-    }
-});
-
-export { }
+export { initSlideshows };
