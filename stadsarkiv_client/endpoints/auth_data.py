167af7b3e02548a3a9dc656ecd8dc51eaec371d3
Dennis Iversen <deiv@aarhus.dk>
2024-01-08 14:40:35 +0100

    test data for proile endpoints

---
+++ stadsarkiv_client/endpoints/auth_data.py
@@ -0,0 +1,53 @@
+api_orders: list = [
+    {
+        "uuid": "abc",
+        "material_id": "000389633",
+        "title": "Kort 889: Signalturforklaringer til 5-farvet kort, Off-service, butikker m.m.",
+        "status": "Completed",
+        "created_at": "2024-01-01 12:00:00",
+        "updated_at": "2024-01-02 12:00:00",
+    },
+    {
+        "uuid": "def",
+        "material_id": "000182514",
+        "title": "Regnskaber (kopier)",
+        "status": "Completed",
+        "created_at": "2024-01-01 12:00:00",
+        "updated_at": "2024-01-02 12:00:00",
+    },
+]
+
+api_booksmarks: list = [
+    {
+        "uuid": "abc",
+        "material_id": "000389633",
+        "title": "Gemt bogmærker 1",
+        "created_at": "2024-01-01 12:00:00",
+        "updated_at": "2024-01-02 12:00:00",
+    },
+    {
+        "uuid": "def",
+        "material_id": "000182514",
+        "title": "Regnskaber (kopier)",
+        "created_at": "2024-01-01 12:00:00",
+        "updated_at": "2024-01-02 12:00:00",
+    },
+]
+
+api_search_results: list = [
+    {
+        "search_query": "test&sort=created_at&order=desc",
+        "title": "Test 1",
+        "created_at": "2024-01-01 12:00:00",
+    },
+    {
+        "search_query": "test&sort=created_at&order=desc",
+        "title": "Test 2",
+        "created_at": "2024-01-01 12:00:00",
+    },
+    {
+        "search_query": "test&sort=created_at&order=desc",
+        "title": "Test 3",
+        "created_at": "2024-01-01 12:00:00",
+    },
+]
