2d14116516e7251806d525294df4c8f1418ae41d
Dennis Iversen <deiv@aarhus.dk>
2024-01-09 10:51:11 +0100

    When upgrading just fetch latest tag if no tag is given.

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
-# This script upgrades the module to a specific tag.
-# Usage: bin/upgrade.sh TAG
+# This script upgrades the module to the latest tag, unless the latest tag is already checked out.
+# Usage: bin/upgrade.sh
 
-# if first argument is empty, exit
-if [ -z "$1" ]
-  then
-    echo "No tag supplied"
-    echo "Usage: bin/upgrade.sh TAG"
+# Fetch tags from the origin
+git fetch --tags
+
+# Get the latest tag
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+# Check if latest tag is empty
+if [ -z "$latest_tag" ]; then
+    echo "No tags found in the repository."
     exit 1
 fi
 
-tag=$1
+# Get the current checked out tag
+current_tag=$(git describe --tags)
+
+# Check if the current tag is the latest
+if [ "$current_tag" = "$latest_tag" ]; then
+    echo "Latest tag ($latest_tag) is already checked out."
+    exit 0
+fi
+
+echo "Upgrading to the latest tag: $latest_tag"
 
 # Stop the service
 sudo systemctl stop stadsarkiv-client.service
 
-# Upgrade the repo and checkout the tag
+# Upgrade the repo and checkout the latest tag
 git checkout main
 git pull
-git checkout $tag
+git checkout $latest_tag
 
 # Activate virtual environment and install requirements
 ./venv/bin/pip install -r requirements.txt
