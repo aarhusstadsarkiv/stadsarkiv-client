ee7dc5db723628e6dce912bd475ea2428e3cd970
Dennis Iversen <dennis.iversen@gmail.com>
2024-01-17 15:24:17 +0100

    mv source

--- resources_port/api_port.py
+++ resources_port/api_port.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
 
 import httpx
 from stadsarkiv_client.core.dynamic_settings import settings
