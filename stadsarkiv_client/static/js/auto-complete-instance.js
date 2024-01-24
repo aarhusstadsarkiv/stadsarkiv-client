03440aa01d644a0889659fc33e4a80d9955fe2b2
Dennis Iversen <deiv@aarhus.dk>
2024-01-24 12:08:08 +0100

    fix auto-complete-instance

--- stadsarkiv_client/static/js/auto-complete-instance.js
+++ stadsarkiv_client/static/js/auto-complete-instance.js
@@ -1,19 +1,15 @@
 import { AutoComplete } from '/static/js/auto-complete.js';
 
-// Render function for autocomplete
-const renderFunction = (data) => {
-
-    // Info if no results
+// Updated render function to accept searchBaseUrl
+const renderFunction = (data, searchBaseUrl) => {
     if (data.length == 0) return `<div class="search-suggestion-info">Ingen forslag. Tryk på søgeknappen for at fritekstsøge i stedet.</div>`;
     
-    // Help text and suggestions
     const suggestionHelp = `<div class="search-suggestion-info">Vælg et forslag nedenfor <strong>eller</strong> tryk på søgeknappen for at fritekstsøge.</div>`;
     
-    // Suggestions
     const suggestions = data.map(item => `
-        <div class="search-suggestion-item" data-url="/search?{{ query_str_display }}${item.domain}=${item.id}">
+        <div class="search-suggestion-item" data-url="${searchBaseUrl}${item.domain}=${item.id}">
             <div>
-                <a href="/search?{{ query_str_display }}${item.domain}=${item.id}">${item.display}</a>
+                <a href="${searchBaseUrl}${item.domain}=${item.id}">${item.display}</a>
             </div>
             <div>${item.sub_display}</div>
         </div>`).join('');
@@ -24,16 +20,16 @@ const renderFunction = (data) => {
 const autocompleteElem = document.querySelector('#q');
 const suggestionsElem = document.querySelector('.search-suggestions');
 
-const options = {
-    'autocompleteElem': autocompleteElem,
-    'suggestionsElem': suggestionsElem,
-    'renderFunction': renderFunction,
-    'endpoint': `/auto_complete?q=`,
-    'minInputLength': 2,
-    'suggestionFocusClass': 'search-suggestion-focus',
-}
+function autoCompleteInit(searchBaseUrl) {
+    const options = {
+        'autocompleteElem': autocompleteElem,
+        'suggestionsElem': suggestionsElem,
+        'renderFunction': (data) => renderFunction(data, searchBaseUrl), // pass searchBaseUrl
+        'endpoint': `/auto_complete?q=`,
+        'minInputLength': 2,
+        'suggestionFocusClass': 'search-suggestion-focus',
+    }
 
-function autoCompleteInit() {
     new AutoComplete(options);
 }
 
