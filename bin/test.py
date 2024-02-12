7857c021b8f1510fc5323166577ba351c39b822a
Dennis Iversen <deiv@aarhus.dk>
2024-02-12 14:17:40 +0100

    rewrite paths

--- bin/test.py
+++ bin/test.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import os
 
+
+# is os is nt when rewrite paths to windows
+def rewrite_path(path):
+    if os.name == "nt":
+        return path.replace("/", "\\")
+    return path
+
+
 # Test default client
-os.system("python -m unittest tests/config-default/*.py")
+os.system(f"python -m unittest {rewrite_path('tests/config-default/*.py')}")
 
 # Test teater client
 os.environ["CONFIG_DIR"] = "example-config-teater"
-os.system("python -m unittest tests/config-teater/*.py")
+os.system(f"python -m unittest {rewrite_path('tests/config-teater/*.py')}")
 
 # Test aarhus client
 os.environ["CONFIG_DIR"] = "example-config-aarhus"
-os.system("python -m unittest tests/config-aarhus/*.py")
+os.system(f"python -m unittest {rewrite_path('tests/config-aarhus/*.py')}")
