3510d20bf06caad5d288c746246130b54c66db1c
Dennis Iversen <deiv@aarhus.dk>
2024-02-12 14:25:26 +0100

    allow to run tests in powershell

--- bin/test.py
+++ bin/test.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import os
+import subprocess
+import glob
 
-# Test default client
-os.system("python -m unittest tests/config-default/*.py")
 
-# Test teater client
-os.environ["CONFIG_DIR"] = "example-config-teater"
-os.system("python -m unittest tests/config-teater/*.py")
+def run_tests(config_dir, tests_path_pattern):
+    if config_dir:
+        os.environ["CONFIG_DIR"] = config_dir
 
-# Test aarhus client
-os.environ["CONFIG_DIR"] = "example-config-aarhus"
-os.system("python -m unittest tests/config-aarhus/*.py")
+    # get test files
+    test_files = glob.glob(tests_path_pattern)
+    if test_files:
+        for test_file in test_files:
+            print(f"Running tests in {test_file}")
+            subprocess.run(["python", "-m", "unittest", test_file], check=True)
+    else:
+        print(f"No tests found matching pattern {tests_path_pattern}")
+
+
+# Example usage
+run_tests(None, "tests/config-default/*.py")
+run_tests("example-config-teater", "tests/config-teater/*.py")
+run_tests("example-config-aarhus", "tests/config-aarhus/*.py")
