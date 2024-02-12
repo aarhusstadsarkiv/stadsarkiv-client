f47f32698c83b1755e7da936abecd427d4a42f52
Dennis Iversen <deiv@aarhus.dk>
2024-02-12 13:29:19 +0100

    rm cli.sh. No needed anymore. Let fix.py run in powershell too

--- bin/cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script is used to run the CLI commands in the module.
-# 
-# For help run without any arguments: ./bin/cli.sh
-
-./venv/bin/python -m stadsarkiv_client $@
--- bin/fix.py
+++ bin/fix.py
@@ -2,7 +2,7 @@
 import os
 
 # This script does linting, type checking, and formatting
-# according to the project's standards. 
+# according to the project's standards.
 os.system("black . --config pyproject.toml")
 os.system("mypy  --config-file pyproject.toml .")
 os.system("flake8 . --config .flake8")
