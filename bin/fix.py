43d6f9f0015373462984cb4a17f102ca08677aac
Dennis Iversen <deiv@aarhus.dk>
2024-02-12 13:39:12 +0100

    execute if __main__ remove old fix.sh

--- bin/fix.py
+++ bin/fix.py
@@ -3,6 +3,9 @@ import os
 
 # This script does linting, type checking, and formatting
 # according to the project's standards.
-os.system("black . --config pyproject.toml")
-os.system("mypy  --config-file pyproject.toml .")
-os.system("flake8 . --config .flake8")
+
+# execute command if __name__ == "__main__"
+if __name__ == "__main__":
+    os.system("black . --config pyproject.toml")
+    os.system("mypy  --config-file pyproject.toml .")
+    os.system("flake8 . --config .flake8")
--- bin/fix.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-
-# This script does linting, type checking, and formatting
-# according to the project's standards. 
-os.system("black . --config pyproject.toml")
-os.system("mypy  --config-file pyproject.toml .")
-os.system("flake8 . --config .flake8")
