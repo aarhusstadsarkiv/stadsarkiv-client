25e98f911dc37e15209e43564c97aaf08b2e9580
Dennis Iversen <deiv@aarhus.dk>
2024-02-22 11:23:13 +0100

    Get relations from local endpoint

--- stadsarkiv_client/endpoints/proxies_relations.py
+++ stadsarkiv_client/endpoints/proxies_relations.py
@@ -1,14 +1,36 @@
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from stadsarkiv_client.core.logging import get_log
+from stadsarkiv_client.core.api import OpenAwsException
+from stadsarkiv_client.core import api
 
 
 log = get_log()
 
 
 async def post(request: Request):
-    # Get form data
-    data = await request.form()
-    log.debug(f"Data: {data}")
+    try:
+        await api.proxies_post_relations(request)
+        return JSONResponse({"error": False, "message": "Relation er oprettet"})
 
-    return JSONResponse({"message": "Hello, World!"})
+    except OpenAwsException as e:
+        log.exception(e)
+        return JSONResponse({"error": True, "message": e.message})
+
+    except Exception as e:
+        log.exception(e)
+        return JSONResponse({"error": True, "message": "Internal Server Error"})
+
+
+async def get(request: Request, type: str, id: str):
+    try:
+        result = await api.proxies_get_relations(request, type, id)
+        return result
+
+    except OpenAwsException as e:
+        log.exception(e)
+        return JSONResponse({"error": True, "message": e.message})
+
+    except Exception as e:
+        log.exception(e)
+        return JSONResponse({"error": True, "message": "Internal Server Error"})
