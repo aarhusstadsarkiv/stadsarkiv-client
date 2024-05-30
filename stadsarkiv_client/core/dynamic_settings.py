6fd34f224493017f9777934b01ec99c1580ae210
Dennis Iversen <deiv@aarhus.dk>
2024-05-30 10:37:17 +0200

    update dynamic_settings doc

--- stadsarkiv_client/core/dynamic_settings.py
+++ stadsarkiv_client/core/dynamic_settings.py
@@ -1,11 +1,16 @@
 """
-Override settings with settings from a config dir.
-Override settings with test settings if TEST is set.
+The dynamic settings will be created based on the following rules:
 
-Merge settings_facets with settings_facets_local (settings_facets_local has precedence)
-Only use facets_enabled from settings. Sort settings_facets by facets_enabled
+Default settings are defined in the settings.py file. These are always loaded.
 
-This is only run one time. When the server is started.
+1) If a config dir is given in the environment variable, the settings will be loaded from that directory.
+2) If a config dir is not given the app will try to look into a directory named 'local'. (This is the default)
+3) If a settings-local.py file exists in the config dir, it will override the settings.
+4) If the environment variable TEST is set, the settings will be overridden by the settings-test.py file.
+(Found in the tests directory)
+
+5) If a settings_facets.py file exists in the config dir, it will override the settings_facets.py file. 
+
 """
 
 from stadsarkiv_client.settings import settings
@@ -19,7 +24,7 @@ log = get_log()
 settings_config: dict = {}
 
 
-# load local settings
+# load local settings (overrides settings)
 try:
     module_name = get_local_config_dir() + ".settings"
     submodule = importlib.import_module(module_name)
@@ -30,7 +35,7 @@ except ImportError:
     log.debug(f"Local settings file NOT loaded: {get_local_config_dir('settings.py')}")
 
 
-# load local settings-dev
+# load local settings-local (overrides settings)
 try:
     module_name = get_local_config_dir() + ".settings-local"
     submodule = importlib.import_module(module_name)
@@ -41,7 +46,7 @@ except ImportError:
     log.debug(f"Local settings file NOT loaded: {get_local_config_dir('settings-local.py')}")
 
 
-# settings for tests
+# load settings for tests (overrides settings)
 if os.getenv("TEST"):
     module_name = "tests.settings-test"
     submodule = importlib.import_module(module_name)
@@ -55,6 +60,7 @@ def get_setting(key):
 
 settings_facets_local: dict = {}
 
+# load local settings_facets (overrides settings_facets)
 try:
     module_name = get_local_config_dir() + ".settings_facets"
     submodule = importlib.import_module(module_name)
