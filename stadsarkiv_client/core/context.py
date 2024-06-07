deba6b4853b052129fffe71b0434de1c77da90a2
Dennis Iversen <deiv@aarhus.dk>
2024-06-07 13:13:27 +0200

    correct code style in context.py

--- stadsarkiv_client/core/context.py
+++ stadsarkiv_client/core/context.py
@@ -17,7 +17,7 @@ from stadsarkiv_client.core import cookie
 log = get_log()
 
 
-async def get_context(request: Request, context_values: dict = {}, identifier: str = '') -> dict:
+async def get_context(request: Request, context_values: dict = {}, identifier: str = "") -> dict:
     hooks = get_hooks(request)
 
     logged_in = await api.is_logged_in(request)
