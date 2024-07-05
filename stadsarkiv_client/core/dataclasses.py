c57058f3a843ef88e7e46f2c8ff336832dc42945
Dennis Iversen <deiv@aarhus.dk>
2024-07-05 13:21:54 +0200

    add SearchCookie dataclass

--- stadsarkiv_client/core/dataclasses.py
+++ stadsarkiv_client/core/dataclasses.py
@@ -9,9 +9,17 @@ import typing
 @dataclasses.dataclass
 class RecordPagination:
     query_str_display: str
-    total: typing.Optional[int] = None
+    total: int = 0
     next_page: typing.Optional[int] = None
     prev_page: typing.Optional[int] = None
     next_record: typing.Optional[int] = None
     prev_record: typing.Optional[int] = None
     current_page: typing.Optional[int] = None
+
+
+@dataclasses.dataclass
+class SearchCookie:
+    query_str_display: str = ""
+    query_params: list = dataclasses.field(default_factory=list)
+    total: int = 0
+    q: str = ""
