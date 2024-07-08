bd1c86024971ec5c5ec724cf98d38c7bb229dbd6
Dennis Iversen <deiv@aarhus.dk>
2024-07-08 11:40:12 +0200

    set bookmarks userdata

--- stadsarkiv_client/core/user_data.py
+++ stadsarkiv_client/core/user_data.py
@@ -25,6 +25,8 @@ class UserData:
         for bookmark in bookmarks:
             bookmark["record_id"] = str(bookmark["record_id"])
 
+        self.data["bookmarks"] = bookmarks
+
     def append_bookmark(self, record_id: str):
         """
         Append a record_id to the bookmarks list.
