a4d7d46a289b29a701f748d5f9f316b497505c2b
Dennis Iversen <deiv@aarhus.dk>
2024-09-12 11:10:51 +0200

    refactor hooks.py a bit

--- stadsarkiv_client/core/hooks.py
+++ stadsarkiv_client/core/hooks.py
@@ -2,47 +2,46 @@ from starlette.requests import Request
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.core.hooks_spec import HooksSpec
 from stadsarkiv_client.core.args import get_local_config_dir
-import importlib
+from stadsarkiv_client.core.module_loader import load_module_attr
 import typing
+import os
 
 
 log = get_log()
 
 
-def _get_local_hooks():
-    """
-    Get local hooks if they exist
-    """
-    module_name = get_local_config_dir() + ".hooks"
-    submodule = importlib.import_module(module_name)
-    HooksLocal = getattr(submodule, "Hooks")
-    return HooksLocal
+# On startup log if hooks will be loaded
+hooks_path = get_local_config_dir("hooks.py")
+if os.path.exists(hooks_path):
+    log.debug(f"Local hooks exists. Will load local hooks {hooks_path}")
+else:
+    log.debug("Local hooks does not exist. Will load default hooks")
 
 
-# debug text to display the type hooks that are loaded on startup. 'local' or 'default'
-try:
-    _get_local_hooks()
-    log.debug(f"Loaded local hooks: {get_local_config_dir('hooks.py')}")
-except ImportError:
-    log.debug(f"Local hooks NOT loaded: {get_local_config_dir('hooks.py')}")
+def _get_mock_request() -> Request:
+    # generate a fake request
+    scope = {"type": "http", "method": "GET", "path": "/", "headers": []}
+
+    async def receive():
+        return {"type": "http.disconnect"}
+
+    request = Request(scope, receive=receive)
+
+    return request
 
 
 def get_hooks(request: typing.Optional[Request] = None) -> HooksSpec:
 
     if not request:
-        # generate a fake request
-        scope = {"type": "http", "method": "GET", "path": "/", "headers": []}
-
-        async def receive():
-            return {"type": "http.disconnect"}
-
-        request = Request(scope, receive=receive)
+        request = _get_mock_request()
 
     """
     Get local hooks if they exist, otherwise get the default hooks
     """
     try:
-        HooksLocal = _get_local_hooks()
+
+        module_name = get_local_config_dir() + ".hooks"
+        HooksLocal = load_module_attr(module_name, "Hooks")
         hooks_local = HooksLocal(request)
         return hooks_local
     except ImportError:
