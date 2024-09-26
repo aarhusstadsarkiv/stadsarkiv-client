740712af893a05395f20cce84ee783ddc9060faa
Dennis Iversen <deiv@aarhus.dk>
2024-09-26 14:31:22 +0200

    rm unused files from locales

--- stadsarkiv_client/locales/test.py
+++
@@ -1,5 +0,0 @@
-da = {
-    "User already exists. Try to login instead.": "Bruger eksisterer allerede. Prøv at logge ind.",
-    "You have been logged out": "You have been logged out",
-    "Login": "Login",
-}
