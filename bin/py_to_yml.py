331dd38eb88f0cbd2d832093e6181c69d8566a6c
Dennis Iversen <deiv@aarhus.dk>
2024-09-26 13:58:08 +0200

    remove conversions already done

--- bin/py_to_yml.py
+++ bin/py_to_yml.py
@@ -3,6 +3,6 @@ import yaml
 
 from stadsarkiv_client.core.module_loader import load_submodule_from_file
 
-settings_facets = load_submodule_from_file("facets", "settings_facets", "example-config-simple/facets.py")
-with open("example-config-simple/facets.yml", "w", encoding="utf8") as file:
-    yaml.dump(settings_facets, file, allow_unicode=True, sort_keys=True)
+# settings_facets = load_submodule_from_file("language", "language", "example-config-simple/language.py")
+# with open("example-config-simple/language.yml", "w", encoding="utf8") as file:
+#     yaml.dump(settings_facets, file, allow_unicode=True, sort_keys=True)
