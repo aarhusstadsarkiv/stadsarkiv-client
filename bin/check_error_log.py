5ff5b1a2227eb8ea72ee75dd6873a0841f219f1d
Dennis Iversen <deiv@aarhus.dk>
2024-10-10 13:50:45 +0200

    iterate all extracted errors urls. to check how many errors are gone.

---
+++ bin/check_error_log.py
@@ -0,0 +1,41 @@
+import httpx
+
+# allow to sleep
+import time
+
+
+urls_file = "data/logs/error_urls.txt"
+
+
+# read the urls from the file
+with open(urls_file, "r") as f:
+    urls = f.readlines()
+
+# print number of urls
+num_urls = len(urls)
+print(f"Found {num_urls} urls")
+
+# remove duplicates
+urls = list(set(urls))
+
+# print number of unique urls
+num_unique_urls = len(urls)
+print(f"Found {num_unique_urls} unique urls")
+
+
+def check_url(url):
+    try:
+        response = httpx.get(url)
+        return response.status_code
+    except httpx.RequestError:
+        return "Request error"
+    except httpx.HTTPStatusError:
+        return "HTTP status error"
+
+
+# check each url
+for url in urls:
+    url = url.strip()
+    status = check_url(url)
+    print(f"{url} - {status}")
+    time.sleep(2)
