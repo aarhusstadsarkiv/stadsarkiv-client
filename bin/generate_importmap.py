97c4cde1a590e69f7eb5858f39a390fa98b2883a
Dennis Iversen <deiv@aarhus.dk>
2024-10-10 09:36:11 +0200

    simplify importmap generation

--- bin/generate_importmap.py
+++ bin/generate_importmap.py
@@ -1,20 +1,24 @@
 import os
 import json
 
+
 def generate_import_map(base_dir):
     js_files = [f for f in os.listdir(base_dir) if f.endswith(".js")]
     import_map = {"imports": {}}
 
     for js_file in js_files:
-        key = "{{ url_for('static', path='/js/" + js_file + "') }}"
+        # key = "{{ url_for('static', path='/js/" + js_file + "') }}"
+        # value = key + "?v={{ get_setting('version') }}"
+        key = f"/static/js/{js_file}"
         value = key + "?v={{ get_setting('version') }}"
-        import_map['imports'][key] = value
+        import_map["imports"][key] = value
 
     return json.dumps(import_map, indent=4)
+
 
 base_directory = "stadsarkiv_client/static/js"
 import_map = generate_import_map(base_directory)
 
-import_map_html = f"<script type=\"importmap\">\n{import_map}\n</script>"
+import_map_html = f'<script type="importmap">\n{import_map}\n</script>'
 with open("stadsarkiv_client/templates/includes/importmap.html", "w") as f:
     f.write(import_map_html)
