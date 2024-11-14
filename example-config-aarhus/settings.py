6450542534dd35ebac3d196d90212147de7bbf92
Dennis Iversen <deiv@aarhus.dk>
2024-11-14 14:54:58 +0100

    rm route from menu as this is not running on live

--- example-config-aarhus/settings.py
+++ example-config-aarhus/settings.py
@@ -46,7 +46,6 @@ settings: dict[str, typing.Any] = {
         {"name": "auth_logout_get", "title": "Log ud"},
         {"name": "auth_register_get", "title": "Ny bruger"},
         {"name": "auth_me_get", "title": "Profil"},
-        {"name": "admin_orders_get", "title": "Bestillinger"},
         {"name": "admin_users_get", "title": "Brugere"},
         {"name": "schemas_get_list", "title": "Skemaer"},
         {"name": "entities_get_list", "title": "Entiteter"},
