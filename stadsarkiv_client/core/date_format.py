0686ce2fc2971f0794cf916452504c79759b4b29
Dennis Iversen <deiv@aarhus.dk>
2024-11-19 12:42:52 +0100

    add def to convert sql ts between timezones rm locale.setlocale

--- stadsarkiv_client/core/date_format.py
+++ stadsarkiv_client/core/date_format.py
@@ -1,21 +1,29 @@
 """
-Format a date string to a Danish format
+Date related functions
 """
 
 from datetime import datetime
 from babel.dates import format_datetime, format_date
-import locale
 from stadsarkiv_client.core.logging import get_log
-
+from zoneinfo import ZoneInfo
 
 log = get_log()
 
 
-locale.setlocale(locale.LC_ALL, "da_DK.UTF-8")
+def convert_utc_to_timezone(utc_timestamp: str, target_timezone: str = "Europe/Copenhagen") -> datetime:
+    """
+    Convert utc timestamp to a target timezone
+    E.g. convert a sql timestamp like '2024-11-19 11:25:58' to 'Europe/Copenhagen' and get e.g. '2024-11-19 12:25:58'
+    """
+    utc_dt = datetime.strptime(utc_timestamp, "%Y-%m-%d %H:%M:%S")
+    utc_dt = utc_dt.replace(tzinfo=ZoneInfo("UTC"))
+    target_dt = utc_dt.astimezone(ZoneInfo(target_timezone))
+    return target_dt
 
 
 def _sanitize_date_string(date_string: str) -> str:
-    """remove microseconds from date string
+    """
+    remove microseconds from date string
     so that it can be parsed by datetime.strptime as "%Y-%m-%dT%H:%M:%S"
     Some dates comes as the format "%Y-%m-%dT%H:%M:%S.%f"
     """
@@ -25,7 +33,9 @@ def _sanitize_date_string(date_string: str) -> str:
 
 
 def date_format(date_string: str) -> str:
-    """Format a date string to a Danish format"""
+    """
+    Format a date string to a Danish format
+    """
     try:
         date_format = "%Y-%m-%dT%H:%M:%S"
         date_string = _sanitize_date_string(date_string)
@@ -38,7 +48,9 @@ def date_format(date_string: str) -> str:
 
 
 def date_format_day(date_string: str) -> str:
-    """Format a date string to a Danish format"""
+    """
+    Format a date string to a Danish format
+    """
     try:
         date_format = "%Y-%m-%d"
         date = datetime.strptime(date_string, date_format)
