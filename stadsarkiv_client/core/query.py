a63110e940dd412cf016c3387031e811fb124d82
Dennis Iversen <deiv@aarhus.dk>
2024-11-20 12:38:06 +0100

    allow to remove_keys from query params need for removing size param

--- stadsarkiv_client/core/query.py
+++ stadsarkiv_client/core/query.py
@@ -10,8 +10,10 @@ log = get_log()
 
 
 def get_list(request: Request, accept_keys: list = [], default_query_params: list = []) -> list:
-    """Get query params from request and return it as a list of tuples.
-    e.g. [('content_types', '96')]"""
+    """
+    Get query params from request and return it as a list of tuples.
+    e.g. [('content_types', '96')]
+    """
     query_params = request.query_params
     items = query_params.multi_items()
 
@@ -21,21 +23,24 @@ def get_list(request: Request, accept_keys: list = [], default_query_params: lis
     return items
 
 
-def get_str_from_list(items: list, accept_keys: list = []) -> str:
+def get_str_from_list(query_params: list, accept_keys: list = [], remove_keys: list = []) -> str:
     """Get list of tuples and return it as a quote plus encoded string."""
 
     # hack to remove leading zeros
     # trim all "0" from items value. Eg. "000096" -> "96" except if value is "0"
-    items = [(key, value.lstrip("0")) if value != "0" else (key, value) for key, value in items]
+    query_params_used = [(key, value.lstrip("0")) if value != "0" else (key, value) for key, value in query_params]
 
     query_str = ""
 
     if accept_keys:
-        items = [(key, value) for key, value in items if key in accept_keys]
+        query_params_used = [(key, value) for key, value in query_params_used if key in accept_keys]
     else:
-        items = [(key, value) for key, value in items]
+        query_params_used = [(key, value) for key, value in query_params_used]
 
-    for key, value in items:
+    if remove_keys:
+        query_params_used = [(key, value) for key, value in query_params_used if key not in remove_keys]
+
+    for key, value in query_params_used:
         value = quote_plus(value)
         query_str += f"{key}={value}&"
 
