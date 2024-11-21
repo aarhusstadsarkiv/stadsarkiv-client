cd8b0e0c30a6f834a784ed09dda1c0e5a38ea9b9
Dennis Iversen <deiv@aarhus.dk>
2024-11-21 14:24:01 +0100

    extends default crud class add spcial methods

--- stadsarkiv_client/database/orders.py
+++ stadsarkiv_client/database/orders.py
@@ -1,6 +1,8 @@
 from stadsarkiv_client.core.dynamic_settings import settings
 from stadsarkiv_client.core.logging import get_log
 from stadsarkiv_client.database.crud import CRUD
+from stadsarkiv_client.database.sql_builder import SQLBuilder
+import sqlite3
 
 log = get_log()
 
@@ -9,4 +11,20 @@ try:
 except KeyError:
     database_url = ""
 
-crud_orders = CRUD(database_url=database_url, table="orders")
+
+class OrdersCRUD(CRUD):
+    def __init__(self, database_url: str, table: str):
+        super().__init__(database_url, table)
+
+    async def order_patch_user(self, update_values: dict, filters: dict):
+        async with self.transaction_scope() as connection:
+            try:
+                sql_builder = SQLBuilder("orders")
+                query = sql_builder.build_update(update_values=update_values, filters=filters)
+                connection.execute(query, sql_builder.values)
+
+            except sqlite3.Error as e:
+                raise e            
+
+
+crud_orders = OrdersCRUD(database_url=database_url, table="orders")
