69f8542bfe4d7c0b3f3d6c34f199ce60ba178131
Dennis Iversen <deiv@aarhus.dk>
2025-01-09 13:33:12 +0100

    add demo key

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -2,6 +2,7 @@ import typing
 
 
 settings: dict[str, typing.Any] = {
+    "key": "demo_e_0_demo",
     "client_name": "development",
     "client_url": "https://demo.openaws.dk",
     "log_handlers": ["stream"],
