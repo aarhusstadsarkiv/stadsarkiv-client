b647093bca66ea26a30d592ffa70d7bf5cf6c609
Dennis Iversen <deiv@aarhus.dk>
2025-01-29 10:44:02 +0100

    note about editing constants

--- stadsarkiv_client/static/js/orders.js
+++ stadsarkiv_client/static/js/orders.js
@@ -1,3 +1,8 @@
+/**
+ * If editing this file, please also update `StatusesUser` in `orders.py`
+ * stadsarkiv_client/database/utils_orders.py
+ */
+
 class StatusesUser {
     static ORDERED = 1;
     static COMPLETED = 2;
