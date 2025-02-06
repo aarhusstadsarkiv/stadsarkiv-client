87788797843e7589ad01ed03e71bb7b7864b328d
Dennis Iversen <deiv@aarhus.dk>
2025-02-06 11:10:07 +0100

    more sql part of migrations to module

---
+++ stadsarkiv_client/migrations/default.py
@@ -0,0 +1,76 @@
+create_booksmarks_query = """
+CREATE TABLE bookmarks (
+    id INTEGER PRIMARY KEY,
+    bookmark TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+) STRICT;
+"""
+
+create_booksmarks_index_query = """
+CREATE INDEX idx_bookmarks_user_id ON bookmarks (user_id);
+"""
+
+create_searches_query = """
+CREATE TABLE searches (
+    id INTEGER PRIMARY KEY,
+    search TEXT NOT NULL,
+    title TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+) STRICT;
+"""
+
+create_searches_index_query = """
+CREATE INDEX idx_searches_user_id ON searches (user_id);
+"""
+
+create_cache_query = """
+CREATE TABLE cache (
+    id INTEGER PRIMARY KEY,
+    key TEXT NOT NULL,
+    value TEXT,
+    unix_timestamp INTEGER DEFAULT 0
+) STRICT;
+"""
+
+create_cache_index_query = """
+CREATE INDEX idx_cache_key ON cache (key);
+"""
+
+create_error_logs = """
+CREATE TABLE error_logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time TEXT,
+    name TEXT,
+    level TEXT,
+    message TEXT,
+    exception TEXT,
+    url TEXT,
+    error_code INTEGER,
+    resolved INTEGER DEFAULT 0,
+    UNIQUE(url, message)
+) STRICT;
+"""
+
+create_error_logs_index = """
+CREATE INDEX idx_time ON error_logs (time);
+"""
+
+# alter bookmarks table bookmark column name to record_id
+alter_bookmarks_table = """
+ALTER TABLE bookmarks RENAME COLUMN bookmark TO record_id;
+"""
+
+# List of migrations with keys
+migrations = {
+    "create_bookmarks": create_booksmarks_query,
+    "create_bookmarks_index": create_booksmarks_index_query,
+    "create_searches": create_searches_query,
+    "create_searches_index": create_searches_index_query,
+    "create_cache": create_cache_query,
+    "create_cache_index": create_cache_index_query,
+    "create_error_logs": create_error_logs,
+    "create_error_logs_index": create_error_logs_index,
+    "alter_bookmarks_table": alter_bookmarks_table,
+}
---
+++ stadsarkiv_client/migrations/orders.py
@@ -0,0 +1,71 @@
+_create_orders_query = """
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    user_email TEXT NOT NULL,
+    user_display_name TEXT NOT NULL,
+    UNIQUE (user_email, user_id)
+) STRICT;
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    user_status INTEGER NOT NULL,
+    record_id TEXT NOT NULL,
+    deadline TEXT,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    comment TEXT DEFAULT "",
+    message_sent INTEGER DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (record_id) REFERENCES records(record_id)
+) STRICT;
+
+CREATE TABLE records (
+    record_id TEXT PRIMARY KEY,
+    label TEXT NOT NULL,
+    meta_data TEXT,
+    record_and_types TEXT,
+    location INTEGER NOT NULL
+) STRICT;
+
+CREATE TABLE orders_log (
+    log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    order_id INTEGER NOT NULL,
+    updated_location INTEGER,
+    updated_user_status INTEGER,
+    updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    record_id TEXT NOT NULL,
+    message TEXT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+) STRICT;
+
+-- Indexes for users
+CREATE INDEX idx_users_user_display_name ON users(user_display_name);
+CREATE INDEX idx_users_user_email ON users(user_email);
+
+-- Indexes for orders
+CREATE INDEX idx_orders_user_id ON orders(user_id);
+CREATE INDEX idx_orders_record_id ON orders(record_id);
+CREATE INDEX idx_orders_user_status ON orders(user_status);
+CREATE INDEX idx_orders_log_updated_at ON orders_log(updated_at);
+CREATE INDEX idx_orders_updated_at ON orders(updated_at);
+
+-- Indexes for records
+CREATE INDEX idx_orders_deadline ON orders(deadline);
+CREATE INDEX idx_records_location ON records(location);
+
+-- Indexes for orders_log
+CREATE INDEX idx_orders_log_order_id ON orders_log(order_id);
+CREATE INDEX idx_orders_log_user_id ON orders_log(user_id);
+CREATE INDEX idx_orders_log_record_id ON orders_log(record_id);
+
+-- INSERT system user in users table
+INSERT INTO users (user_id, user_email, user_display_name) VALUES ('SYSTEM', 'system', 'SYSTEM');
+
+"""
+
+# List of migrations with keys
+migrations = {
+    "create_orders": _create_orders_query,
+}
