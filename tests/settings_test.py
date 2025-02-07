213b8a91d1aee4d986295a31ed2e79a027e1bd00
Dennis Iversen <deiv@aarhus.dk>
2025-02-07 13:46:28 +0100

    clean up settings_test

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -6,19 +6,8 @@ settings: dict[str, typing.Any] = {
     "client_url": "https://demo.openaws.dk",
     "log_handlers": ["stream"],
     "api_base_url": "https://dev.openaws.dk/v1",
-    # "sqlite3": {
-    #     "orders": "data/orders.db",
-    #     # memory version
-    #     "orders_test": ":memory:",
-    # },
-
     "sqlite3": {
         "default": "/tmp/database.db",
         "orders": "/tmp/orders.db",
     },
-
-    # "sqlite3": {
-    #     "default": ":memory:",
-    #     "orders": ":memory:",
-    # },
 }
