8f0431b9af56597d9024e829d8153ec523d22592
Dennis Iversen <deiv@aarhus.dk>
2025-02-18 14:35:38 +0100

    add salling hooks

--- example-config-salling/hooks.py
+++ example-config-salling/hooks.py
@@ -32,13 +32,21 @@ class Hooks(HooksSpec):
         Alter the search query params. Before the search is executed.
         This example removes all curators from the query params and adds Aarhus Teater as curator (4).
         """
+        # Remove all curators from the query params and add curator (4)
+        query_params = [(key, value) for key, value in query_params if key != "organisations"]
+        query_params.append(("organisations", "107434"))
 
         return query_params
 
     async def after_get_search(self, query_params: list) -> list:
         """
         Alter the search query params. After the search is executed.
+        This example removes all curators from the query params.
+        This is done to avoid that the curator added in the before_search method is added to filters and search cookie.
         """
+        # organisations=107434
+        query_params = [(key, value) for key, value in query_params if key != "organisations"]
+
         return query_params
 
     async def after_get_record(self, record: dict, meta_data: dict) -> tuple:
