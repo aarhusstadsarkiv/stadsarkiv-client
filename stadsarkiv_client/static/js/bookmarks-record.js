6c3e25885125496ccfded60a8dcb2726e919e4c2
Dennis Iversen <deiv@aarhus.dk>
2025-02-25 14:30:34 +0100

    check for selector.

--- stadsarkiv_client/static/js/bookmarks-record.js
+++ stadsarkiv_client/static/js/bookmarks-record.js
@@ -5,57 +5,61 @@ import { Flash } from "/static/js/flash.js";
 
 let action;
 const bookmarkAddElem = document.getElementById('bookmark-action');
-const recordId = bookmarkAddElem.getAttribute('data-id');
-const spinner = document.querySelector('.loadingspinner');
+if (bookmarkAddElem) {
+    const recordId = bookmarkAddElem.getAttribute('data-id');
+    const spinner = document.querySelector('.loadingspinner');
 
-/**
- * Load bookmarks
- * Initialize bookmark button
- */
-const initialize = async () => {
-    const url = `/auth/bookmarks/json?record_id=${recordId}`;
-    let bookmarksJSON = await Requests.asyncGetJson(url, 'GET');
-    if (Object.keys(bookmarksJSON).length !== 0) {
-        bookmarkAddElem.innerHTML = 'Fjern bogmærke';
-        action = 'remove';
-        bookmarkAddElem.setAttribute('data-action', action);
-    } else {
-        bookmarkAddElem.innerHTML = 'Tilføj bogmærke';
-        action = 'add';
-        bookmarkAddElem.setAttribute('data-action', action);
-    }
-}
-
-await initialize();
-
-/**
- * Get data for record to be bookmarked
- */
-const get_data = () => {
-    const data = { record_id: recordId, action: action };
-    return JSON.stringify(data);
-}
-
-/**
- * Add or remove bookmark
- */
-bookmarkAddElem.addEventListener('click', async function (e) {
-    let res;
-    e.preventDefault();
-    spinner.classList.toggle('hidden');
-    try {
-        const data = get_data();
-        res = await Requests.asyncPostJson('/auth/bookmarks', data, 'POST');
-        if (res.error) {
-            Flash.setMessage(res.message, 'error');
+    /**
+     * Load bookmarks
+     * Initialize bookmark button
+     */
+    const initialize = async () => {
+        const url = `/auth/bookmarks/json?record_id=${recordId}`;
+        let bookmarksJSON = await Requests.asyncGetJson(url, 'GET');
+        if (Object.keys(bookmarksJSON).length !== 0) {
+            bookmarkAddElem.innerHTML = 'Fjern bogmærke';
+            action = 'remove';
+            bookmarkAddElem.setAttribute('data-action', action);
         } else {
-            Flash.setMessage(res.message, 'success');
+            bookmarkAddElem.innerHTML = 'Tilføj bogmærke';
+            action = 'add';
+            bookmarkAddElem.setAttribute('data-action', action);
         }
-    } catch (e) {
-        Flash.setMessage(res.message, 'error');
-        await asyncLogError('/error/log', e.stack);
-    } finally {
-        initialize();
-        spinner.classList.toggle('hidden');
     }
-});
+
+    await initialize();
+
+    /**
+     * Get data for record to be bookmarked
+     */
+    const get_data = () => {
+        const data = { record_id: recordId, action: action };
+        return JSON.stringify(data);
+    }
+
+    /**
+     * Add or remove bookmark
+     */
+    bookmarkAddElem.addEventListener('click', async function (e) {
+        let res;
+        e.preventDefault();
+        spinner.classList.toggle('hidden');
+        try {
+            const data = get_data();
+            res = await Requests.asyncPostJson('/auth/bookmarks', data, 'POST');
+            if (res.error) {
+                Flash.setMessage(res.message, 'error');
+            } else {
+                Flash.setMessage(res.message, 'success');
+            }
+        } catch (e) {
+            Flash.setMessage(res.message, 'error');
+            await asyncLogError('/error/log', e.stack);
+        } finally {
+            initialize();
+            spinner.classList.toggle('hidden');
+        }
+    });
+}
+
+
