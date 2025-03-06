cc2624c6ce2210e31d42e41d7572d517d9664b0f
Dennis Iversen <deiv@aarhus.dk>
2025-03-06 14:08:44 +0100

    rm record content in settings. style a bit

--- example-config-salling/settings.py
+++ example-config-salling/settings.py
@@ -42,4 +42,5 @@ settings: dict[str, typing.Any] = {
     "allow_save_bookmarks": False,
     "allow_save_search": False,
     "allow_online_ordering": False,
+    "ignore_record_keys": ["curators", "collectors", "collection", "organisations"],
 }
