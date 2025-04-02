4be2f3ecd2749d98dd3f800708cd0c4454733f92
Dennis Iversen <deiv@aarhus.dk>
2025-04-02 11:03:06 +0200

    better logging of js errors

--- stadsarkiv_client/endpoints/endpoints_error.py
+++ stadsarkiv_client/endpoints/endpoints_error.py
@@ -12,11 +12,12 @@ log = get_log()
 async def error_log_post(request: Request):
     """
     Log posted json data
+    Note: error_type, error_code, error_url are options in the python logger
+    May use them at some point
     """
-
     try:
         data = await request.json()
-        log.error(data)
+        log.error(data.get("message", "JS ERROR"), extra={"exception": data.get("exception", "")})
         return JSONResponse({"status": "received"}, status_code=200)
     except Exception:
         log.error("No json data in request")
