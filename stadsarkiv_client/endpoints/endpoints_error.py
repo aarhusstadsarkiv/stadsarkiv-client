03e04735122b4eeb886c6380c596cdbb20e57979
Dennis Iversen <deiv@aarhus.dk>
2025-04-02 13:43:28 +0200

    log more on endpoints_error

--- stadsarkiv_client/endpoints/endpoints_error.py
+++ stadsarkiv_client/endpoints/endpoints_error.py
@@ -17,7 +17,27 @@ async def error_log_post(request: Request):
     """
     try:
         data = await request.json()
-        log.error(data.get("message", "JS ERROR"), extra={"exception": data.get("exception", "")})
+
+        message = data.get("message", "")
+        error_code = data.get("error_code", "")
+        error_type = data.get("error_type", "")
+        error_url = data.get("error_url", "")
+        exception = data.get("exception", "")
+
+        log.debug(f"Error url: {error_url}")
+
+        # Compose extra fields for logging. Only add them if they are not empty
+        extra = {}
+        if error_code:
+            extra["error_code"] = error_code
+        if error_type:
+            extra["error_type"] = error_type
+        if error_url:
+            extra["error_url"] = error_url
+        if exception:
+            extra["exception"] = exception
+
+        log.error(message, extra=extra)
         return JSONResponse({"status": "received"}, status_code=200)
     except Exception:
         log.error("No json data in request")
