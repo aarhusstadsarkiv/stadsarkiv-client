b68f16906029fa54bcc48e7f550b21a42dcfa44b
Dennis Iversen <deiv@aarhus.dk>
2025-05-09 12:35:31 +0200

    fix tests

--- tests/core/format_date_test.py
+++ tests/core/format_date_test.py
@@ -10,19 +10,19 @@ log = get_log()
 
 class TestDateFunctions(unittest.TestCase):
 
-    @patch("stadsarkiv_client.core.date_format.log")
+    @patch("maya.core.date_format.log")
     def test_timezone_alter(self, mock_log):
         self.assertEqual(timezone_alter("2024-11-19 11:25:58", "Europe/Copenhagen"), "2024-11-19 12:25:58")
         self.assertEqual(timezone_alter("invalid-date", "Europe/Copenhagen"), "invalid-date")
         mock_log.exception.assert_called_once_with("Error in timezone_alter")
 
-    @patch("stadsarkiv_client.core.date_format.log")
+    @patch("maya.core.date_format.log")
     def test_date_format(self, mock_log):
         self.assertEqual(date_format("2024-11-19T11:25:58"), "19. november 2024 11:25")
         self.assertEqual(date_format("invalid-date"), "invalid-date")
         mock_log.exception.assert_called_once_with("Error in date_format")
 
-    @patch("stadsarkiv_client.core.date_format.log")
+    @patch("maya.core.date_format.log")
     def test_date_format_day(self, mock_log):
         self.assertEqual(date_format_day("2024-11-19"), "19. november 2024")
         self.assertEqual(date_format_day("invalid-date"), "invalid-date")
