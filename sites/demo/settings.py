3e1c8dfd8980d1fd6d7498c3bd838db506886ca0
Dennis Iversen <deiv@aarhus.dk>
2025-05-13 14:27:05 +0200

    update demo settings

--- sites/demo/settings.py
+++ sites/demo/settings.py
@@ -23,16 +23,14 @@ settings: dict[str, typing.Any] = {
     "main_menu_system": [
         {"name": "auth_login_get", "title": "Log ind"},
         {"name": "auth_logout_get", "title": "Log ud"},
-        # {"name": "auth_register_get", "title": "Ny bruger"},
+        {"name": "auth_register_get", "title": "Ny bruger"},
         {"name": "auth_me_get", "title": "Profil"},
-        # {"name": "admin_users_get", "title": "Brugere"},
-        # {"name": "schemas_get_list", "title": "Skemaer"},
-        # {"name": "entities_get_list", "title": "Entiteter"},
+        {"name": "admin_users_get", "title": "Brugere"},
+        {"name": "schemas_get_list", "title": "Skemaer"},
+        {"name": "entities_get_list", "title": "Entiteter"},
     ],
-    "sqlite3": {
-        "default": "data/database.db",
-        "orders": "data/orders.db",
-    },
     "search_base_url": "/search",
     "show_version": True,
+    "allow_user_registration": True,
+    "allow_user_management": True,
 }
