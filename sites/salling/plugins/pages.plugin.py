32202327ba80f7e2c3931e740d6cc05eb9096c5c
Dennis Iversen <deiv@aarhus.dk>
2025-05-15 09:09:31 +0200

    remove deprecated story variable from frontpage

--- sites/salling/plugins/pages.plugin.py
+++ sites/salling/plugins/pages.plugin.py
@@ -199,8 +199,6 @@ async def home_page(request: Request):
 
     stories = await story_by_index(index=[])
 
-    story = stories[3]
-
     story_sections = []
     for story in stories:
         story_sections.append(story["first_section"])
@@ -212,7 +210,6 @@ async def home_page(request: Request):
         request,
         context_values={
             "title": "Forside",
-            "story": story,
             "stories": story_sections,
             "memories": memories,
         },
