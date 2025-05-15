0ac1eccc3980d14c3f82ecfb920d0cf12c5c6dad
Dennis Iversen <deiv@aarhus.dk>
2025-05-15 10:09:05 +0200

    mv migration scripts to bin

--- bin/migrate_default.py
+++ bin/migrate_default.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 """
 
-sqlite3 migrations for orders.
+migrations if the system needs to contain:
+- Bookmarks
+- Searches
 
 """
-
 import sys
 
 sys.path.append(".")
 
 from maya.core.dynamic_settings import settings
 from maya.core.migration import Migration
-from maya.migrations.orders import migrations_orders
+from maya.migrations.default import migrations_default
 from maya.core.logging import get_log
 import os
 
@@ -21,14 +22,17 @@ if "BASE_DIR" not in os.environ:
     print("export BASE_DIR=sites/aarhus")
     exit(1)
 
+
 log = get_log()
 
+
 try:
-    db_path = settings["sqlite3"]["orders"]
+    db_path = settings["sqlite3"]["default"]
+    log.info(f"Using database path: {db_path}")
 except KeyError:
     log.error("No database URL found in settings")
     exit(1)
 
-migration_manager = Migration(db_path, migrations_orders)
+migration_manager = Migration(db_path, migrations_default)
 migration_manager.run_migrations()
 migration_manager.close()
--- bin/migrate_orders.py
+++ bin/migrate_orders.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 """
 
-migrations if the system needs to contain:
-- Bookmarks
-- Searches
+sqlite3 migrations for orders.
 
 """
+
 import sys
 
 sys.path.append(".")
 
 from maya.core.dynamic_settings import settings
 from maya.core.migration import Migration
-from maya.migrations.default import migrations_default
+from maya.migrations.orders import migrations_orders
 from maya.core.logging import get_log
 import os
 
@@ -22,16 +21,15 @@ if "BASE_DIR" not in os.environ:
     print("export BASE_DIR=sites/aarhus")
     exit(1)
 
-
 log = get_log()
 
-
 try:
-    db_path = settings["sqlite3"]["default"]
+    db_path = settings["sqlite3"]["orders"]
+    log.info(f"Using database path: {db_path}")
 except KeyError:
     log.error("No database URL found in settings")
     exit(1)
 
-migration_manager = Migration(db_path, migrations_default)
+migration_manager = Migration(db_path, migrations_orders)
 migration_manager.run_migrations()
 migration_manager.close()
