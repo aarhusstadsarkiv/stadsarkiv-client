25cfd79a03ff64dec0065ace4e092ddabfe6d0b7
Dennis Iversen <deiv@aarhus.dk>
2025-05-20 12:20:24 +0200

    simplify settings

--- sites/demo-readonly/settings.py
+++ sites/demo-readonly/settings.py
@@ -7,13 +7,6 @@ settings: dict[str, typing.Any] = {
     "client_email": "stadsarkivet@aarhusarkivet.dk",
     "language": "da",
     "log_handlers": ["rotating_file"],
-    "cookie": {
-        "name": "session",
-        "lifetime": 3600 * 24,  # seconds
-        "httponly": True,
-        "secure": True,
-        "samesite": "lax",
-    },
     "api_base_url": "https://dev.openaws.dk/v1",
     "main_menu_top": [
         {"name": "search_get", "title": "Søg", "type": "icon", "icon": "search"},
